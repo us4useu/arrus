f0a1757ee1ae5075ab770f52a68775f3ffbc90a4
zkLog <ziemowit.klimonda@us4us.eu>
2019-11-06 15:49:42 +0100

    Function added - loading data from simulation (fieldII).

--- python/bmode/reconstructruct_image.py
+++ python/bmode/reconstructruct_image.py
@@ -34,3 +34,54 @@ def reconstruct_rf_img(rf, x_grid, z_grid,
     n_sample, n_channel, n_transmission  = rf.shape
     z_size	= len(z_grid)
     x_size	= len(x_grid)
+
+
+def load_simulated_data(file):
+    """
+
+    dokonczyc
+    :param file:
+    :return:
+    """
+
+
+    matlab_data = sio.loadmat(file)
+    c = matlab_data.get('sos')
+    c = np.float(c)
+
+    fc = matlab_data.get('fn')
+    fc = np.float(fc)
+
+    fs = matlab_data.get('fs')
+    fs = np.float(fs)
+
+    n_elements = matlab_data.get('nElem')
+    n_elements = np.int(n_elements)
+
+    tx_aperture = matlab_data.get('txAp')
+    tx_aperture = np.int(tx_aperture)
+
+    pulse_periods = matlab_data.get('nPer')
+    pulse_periods = np.int(pulse_periods)
+
+    pitch = matlab_data.get('pitch')
+    pitch = np.float(pitch)
+
+    tx_focus = matlab_data.get('txFoc')
+    tx_focus = np.float(tx_focus)
+
+    tx_angle = matlab_data.get('txAng')
+    tx_angle = np.int(tx_angle)
+
+    rf = matlab_data.get('rfLin')
+
+    return [rf, c, fs, fc, pitch, tx_focus, tx_angle, tx_aperture, n_elements, pulse_periods]
+
+
+
+
+
+file = '/home/linuser/us4us/usgData/rfLin_field.mat'
+simdata = load_simulated_data(file)
+print(simdata[7])
+
