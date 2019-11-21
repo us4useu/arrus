e23b56a80da463f3818be91ed12d396cbe788e3c
Piotr Jarosik <jarosik.piotr@gmail.com>
2019-11-21 17:34:22 +0100

    Implemented device discovery for interactive session, refs #7.

--- arius/python/session.py
+++ arius/python/session.py
@@ -5,6 +5,7 @@ import yaml
 import arius.python.device as _device
 import arius.python.interface as _interface
 import arius.python.utils as _utils
+import arius.python.iarius as _iarius
 
 
 _ARIUS_PATH_ENV = "ARIUS_PATH"
@@ -12,18 +13,26 @@ _ARIUS_PATH_ENV = "ARIUS_PATH"
 
 class InteractiveSession:
     def __init__(self):
-        self._dll = self._load_arius_library("Arius.dll")
         self._devices = self._load_devices("default.yaml")
 
-    def get_device(self, name: str):
-        dev_path = name.split("/")[1:]
+    def get_device(self, id: str):
+        """
+        Returns device from given path.
+
+        Currently, ONLY TOP-LEVEL DEVICES ARE AVAILABLE.
+
+        :param a path to a device
+        :return: a device located in given path.
+        """
+        dev_path = id.split("/")[1:]
         if len(dev_path) != 1:
             raise ValueError(
-              "Invalid path, currently top-level devices can be accessed only.")
+              "Invalid path, top-level devices can be accessed only.")
         dev_id = dev_path[0]
         return self._devices[dev_id]
 
-    def _load_devices(self, cfg_file: str, dll):
+    @staticmethod
+    def _load_devices(cfg_file: str):
         """
         Reads configuration from given file and returns a map of top-level
         devices.
@@ -38,8 +47,17 @@ class InteractiveSession:
         path = os.path.join(os.environ[_ARIUS_PATH_ENV], cfg_file)
         with open(path, "r") as f:
             cfg = yaml.safe_load(f)
-        for i, probe_def in enumerate(cfg):
-            name, definition = next(iter(probe_def.items()))
+
+        n_arius_cards = cfg["nAriusCards"]
+        arius_handles = (_iarius.Arius(i) for i in range(n_arius_cards))
+        arius_handles = sorted(arius_handles, key=lambda a: a.GetId())
+        arius_cards = [_device.AriusCard(i, h) for i, h in enumerate(arius_handles)]
+        for card in arius_cards:
+            result[card.get_id()] = card
+
+        probes = cfg['probes']
+        for i, probe_def in enumerate(probes):
+            model_name, definition = next(iter(probe_def.items()))
             interface_name = definition['interface']
             apertures = definition['aperture']
             interface = _interface.get_interface(interface_name)
@@ -47,27 +65,29 @@ class InteractiveSession:
             tx_mappings = interface.get_tx_channel_mapping()
             rx_mappings = interface.get_rx_channel_mapping()
 
-
-            for i, aperture, tx_m, rx_m in zip(order, apertures,
-                                               tx_mappings, rx_mappings):
-
-                # TODO(pjarosik) is it necessary to store card id in cfg?
+            hw_subapertures = []
+            for card_nr, aperture, tx_m, rx_m in zip(order, apertures,
+                                                     tx_mappings, rx_mappings):
                 _utils.assert_true(
-                    i == aperture["card"],
+                    card_nr == aperture["card"],
                     "Card mapping order corresponds to the order defined in cfg."
                 )
-                card_ptr = self._dll.GetArius(i)
-                # TODO(pjarosik) use swig
-
-
-            probe_dependencies = []
-
+                arius_card = arius_cards[card_nr]
+                arius_card.set_tx_channel_mapping(tx_m)
+                arius_card.set_rx_channel_mapping(rx_m)
+                hw_subapertures.append(
+                    _device.ProbeHardwareSubaperture(
+                        arius_card,
+                        aperture["origin"],
+                        aperture["size"]
+                ))
             probe = _device.Probe(
                 index=i,
-                model_name=name,
-                dependencies=probe_dependencies
+                model_name=model_name,
+                hw_subapertures=hw_subapertures
             )
             result[probe.get_id()] = probe
+        return result
 
     @staticmethod
     def _load_arius_library(name: str):
