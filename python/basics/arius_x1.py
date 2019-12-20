bddaacf4a047e69fb4ec3be62cf5485a0f02dd5f
Piotr Jarosik <jarosik.piotr@gmail.com>
2019-12-20 16:46:46 +0100

    Fixed arius_x1 example.

--- python/basics/arius_x1.py
+++ python/basics/arius_x1.py
@@ -2,28 +2,32 @@ import arius as ar
 import numpy as np
 import matplotlib.pyplot as plt
 import sys
+import time
 
 # Start new session with the device.
-sess = ar.session.InteractiveSession()
+sess = ar.session.InteractiveSession("cfg.yaml")
 module = sess.get_device("/Arius:0")
-module.start_if_necessary()
-
 # Configure module's adapter.
 interface = ar.interface.get_interface("esaote")
-module.set_tx_mapping(interface.get_tx_channel_mapping(0))
-module.set_rx_mapping(interface.get_rx_channel_mapping(0))
+module.store_mappings(
+    interface.get_tx_channel_mapping(0),
+    interface.get_rx_channel_mapping(0)
+)
+# Start the device.
+module.start_if_necessary()
 
 # Configure parameters, that will not change later in the example.
-module.set_pga_gain(30) # [dB]
-module.set_lpf_cutoff(10e6) # [Hz]
+module.set_pga_gain(30)  # [dB]
+module.set_lpf_cutoff(10e6)  # [Hz]
 module.set_active_termination(200)
-module.set_lna_gain(24) #[dB]
-module.set_dtgc(0) # [dB]
+module.set_lna_gain(24)  # [dB]
+module.set_dtgc(0)  # [dB]
 
 # Configure TX/RX scheme.
 NEVENTS = 4
 NSAMPLES = 8192
-delays = np.array([i*0.001e-6 for i in range(module.get_n_tx_channels())])
+NCHANELS = module.get_n_rx_channels()
+delays = np.array([i * 0.001e-6 for i in range(module.get_n_tx_channels())])
 for i in range(NEVENTS):
     module.set_tx_delays(delays=delays, firing=i)
     module.set_tx_frequency(frequency=5e6, firing=i)
@@ -31,34 +35,36 @@ for i in range(NEVENTS):
     module.set_tx_aperture(origin=0, size=128, firing=i)
 
     module.set_rx_time(time=200e-6, firing=i)
-    module.set_rx_aperture(origin=i*32, size=32, firing=i)
-    module.schedule_receive(i*NSAMPLES, NSAMPLES)
+    module.set_rx_aperture(origin=i * 32, size=32, firing=i)
+    module.schedule_receive(i * NSAMPLES, NSAMPLES)
 
-module.set_number_of_firings(NEVENTS-1)
+module.set_number_of_firings(NEVENTS)
 module.enable_transmit()
 
 # Run the scheme:
 # - prepare figure to display,
-buffer = np.zeros((NEVENTS, NSAMPLES, module.get_n_rx_channels()), dtype=np.int16)
-rf = np.zeros((NSAMPLES, module.get_n_rx_channels*NEVENTS), dtype=np.int16)
+rf = np.zeros((NSAMPLES, module.get_n_rx_channels() * NEVENTS), dtype=np.int16)
 
 # - matplotlib related stuff
 fig, ax = plt.subplots()
 fig.set_size_inches((7, 7))
 ax.set_xlabel("Channels")
 ax.set_ylabel("Samples")
+ax.set_aspect("auto")
 fig.canvas.set_window_title("RF data")
 
-# -- setting window close event handler
 is_closed = False
+
 
 def set_closed(_):
     global is_closed
     # Intentionally not using threading.Lock (or similar) objects here.
     is_closed = True
+
+
 fig.canvas.mpl_connect("close_event", set_closed)
 
-# -- creating and starting canvas
+# -- create and start canvas
 canvas = plt.imshow(
     rf,
     vmin=np.iinfo(np.int16).min,
@@ -66,23 +72,27 @@ canvas = plt.imshow(
 )
 fig.show()
 
-while is_closed:
+while not is_closed:
+    start = time.time()
     module.enable_receive()
 
-    # - start the acquisition
+    # - acquire RF data
     for i in range(NEVENTS):
         module.sw_trigger()
         module.sw_next_tx()
 
-    # - transfer acquired data
-    module.transfer_rx_buffer_to_host(buffer, 0)
+    # - transfer data from module's internal memory to the host memory
+    buffer = module.transfer_rx_buffer_to_host(0, NEVENTS * NSAMPLES)
 
     # - reorder acquired data
     for i in range(NEVENTS):
-        rf[:, i*32:(i+1)*32] =  buffer[i, :, :]
+        rf[:, i * NCHANELS:(i+1)*NCHANELS] = buffer[i*NSAMPLES:(i+1)*NSAMPLES,:]
 
+    end = time.time()
+    print("Acq time: %.3f" % (end - start), end="\r")
     # - display data
     canvas.set_data(rf)
+    ax.set_aspect("auto")
     fig.canvas.flush_events()
     plt.draw()
 
