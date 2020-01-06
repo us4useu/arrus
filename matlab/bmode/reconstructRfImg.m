bc88156398dcdb953bd9371d35106b7322c16d84
PiotrKarwat <piotr.karwat@us4us.eu>
2020-01-06 22:32:17 +0100

    Reconstruction speed-up
    Use of MATLAB interp1 & interp2 functions makes reconstruction faster if GPU is used and for-loops are avoided.

--- matlab/bmode/reconstructRfImg.m
+++ matlab/bmode/reconstructRfImg.m
@@ -21,21 +21,22 @@ function[rfBfr] = reconstructRfImg(rfRaw,sys,acq,proc)
 % acq.tx.focus              - [m] focal length for STA scheme
 % acq.tx.angle              - [rad] tilting angles for PWI scheme
 % 
-% proc.speedOfSound         - [m/s] speed of sound used in reconstruction
+    % proc.speedOfSound         - [m/s] speed of sound used in reconstruction
 % proc.ddc.decimation       - [] decimation factor
 % proc.ddc.iqEnable         - [logical] 
 % proc.das.xGrid            - [m] (1,xSize) x-grid vector for output rf image
 % proc.das.zGrid            - [m] (1,zSize) z-grid vector for output rf image
 
 % TODO
-% focDel, firstSampShift
+% focDel, firstSampShift, gpu
 fstSampShift = 0;	% [samp] number of the sample that reflects the tx start
 
-[nSamp,nRx,nTx] = size(rfRaw);
+[~,nRx,nTx] = size(rfRaw);
 zSize	= length(proc.das.zGrid);
 xSize	= length(proc.das.xGrid);
 
 xElem	= (-(sys.nElements-1)/2:(sys.nElements-1)/2)*sys.pitch;
+xElem	= permute(xElem,[1 3 2]);
 
 fs      = acq.rx.samplingFrequency/proc.ddc.decimation;
 
@@ -51,68 +52,91 @@ else
 end
 initDel     = - fstSampDel + focDel + burstFactor;                      % [s] total init delay
 
+%% GPU memory allocation
+if isa(rfRaw,'gpuArray')
+    xElem           = gpuArray(xElem);
+    proc.das.zGrid	= gpuArray(proc.das.zGrid);
+    proc.das.xGrid	= gpuArray(proc.das.xGrid);
+    
+    rfTx	= zeros(zSize,xSize,nTx,'gpuArray');
+    wghTx	= zeros(zSize,xSize,nTx,'gpuArray');
+else
+    rfTx	= zeros(zSize,xSize,nTx);
+    wghTx	= zeros(zSize,xSize,nTx);
+end
+
+%% Precalculate tx delays and apodization
+switch acq.mode
+    case 'sta'
+        % synthetic transmit aperture method
+        txDist	= sqrt((proc.das.zGrid' - acq.tx.focus).^2 + (proc.das.xGrid-xElem).^2);
+        txDist	= txDist.*sign(proc.das.zGrid' - acq.tx.focus) + acq.tx.focus;          % WARNING: sign()=0 => invalid txDist value
+        
+        txFNum	= abs((proc.das.xGrid-xElem)) ./ max(abs(proc.das.zGrid' - acq.tx.focus),1e-12);
+        txApod	= double(txFNum < 0.5);
+        
+    case 'pwi'
+        % plane wave imaging method
+        txAng	= permute(acq.tx.angle,[1 3 2]);
+        eFst	= 1 + (sys.nElements-1)*(txAng<0);
+        
+        txDist	= (proc.das.xGrid-xElem(eFst))*sin(txAng) + proc.das.zGrid'*cos(txAng);
+        
+        r1      = (proc.das.xGrid-xElem(   1))*cos(txAng) - proc.das.zGrid'*sin(txAng);
+        r2      = (proc.das.xGrid-xElem( end))*cos(txAng) - proc.das.zGrid'*sin(txAng);
+        txApod	= double(r1 >= 0 & r2 <= 0);
+        
+end
+
+%% Precalculate rx delays and apodization
+rxDist	= sqrt((proc.das.xGrid-xElem).^2 + proc.das.zGrid'.^2);
+rxFNum	=  abs((proc.das.xGrid-xElem)   ./ proc.das.zGrid');
+rxApod	= rxFNum < 0.5;
+
+iRx     = repmat(permute(1:nRx,[1 3 2]),[zSize xSize 1]);
+
 %% Delay & Sum
-% make last samples = 0. If the algorithm needs a sample which is out of range 1:(nSamp-1), 
-% then it uses the sample no. nSamp which is 0.
-% Option of appending zeros as last samples instead of making last sample equal zero is slower.
-rfRaw(end,:,:)	= 0;
-
-rfTx	= zeros(zSize,xSize,nTx);
-wghTx	= zeros(zSize,xSize,nTx);
-
 wb = waitbar(0,'rf image reconstruction');
 for iTx=1:nTx
     
-    % calculate tx delays and apodization
-    switch acq.mode
-        case 'sta'
-            % synthetic transmit aperture method
-            txDist	= sqrt((proc.das.zGrid' - acq.tx.focus).^2 + (proc.das.xGrid-xElem(iTx)).^2);
-            txDist	= txDist.*sign(proc.das.zGrid' - acq.tx.focus) + acq.tx.focus;          % WARNING: sign()=0 => invalid txDist value
-            
-            fNum	= abs((proc.das.xGrid-xElem(iTx))) ./ max(abs(proc.das.zGrid' - acq.tx.focus),1e-12);
-            txApod	= double(fNum < 0.5);
-            
-        case 'pwi'
-            % plane wave imaging method
-            if acq.tx.angle(iTx) >= 0
-                eFst	= 1;
-            else
-                eFst	= sys.nElements;
-            end
-            txDist	= (proc.das.xGrid-xElem(eFst))*sin(acq.tx.angle(iTx)) + proc.das.zGrid'*cos(acq.tx.angle(iTx));
-            
-            r1      = (proc.das.xGrid-xElem(   1))*cos(acq.tx.angle(iTx)) - proc.das.zGrid'*sin(acq.tx.angle(iTx));
-            r2      = (proc.das.xGrid-xElem( end))*cos(acq.tx.angle(iTx)) - proc.das.zGrid'*sin(acq.tx.angle(iTx));
-            txApod	= double(r1 >= 0 & r2 <= 0);
-            
-    end
+%                     % calculate tx delays and apodization
+%                     switch acq.mode
+%                         case 'sta'
+%                             % synthetic transmit aperture method
+%                             txDist	= sqrt((proc.das.zGrid' - acq.tx.focus).^2 + (proc.das.xGrid-xElem(iTx)).^2);
+%                             txDist	= txDist.*sign(proc.das.zGrid' - acq.tx.focus) + acq.tx.focus;          % WARNING: sign()=0 => invalid txDist value
+% 
+%                             txFNum	= abs((proc.das.xGrid-xElem(iTx))) ./ max(abs(proc.das.zGrid' - acq.tx.focus),1e-12);
+%                             txApod	= double(txFNum < 0.5);
+% 
+%                         case 'pwi'
+%                             % plane wave imaging method
+%                             if acq.tx.angle(iTx) >= 0
+%                                 eFst	= 1;
+%                             else
+%                                 eFst	= sys.nElements;
+%                             end
+%                             txDist	= (proc.das.xGrid-xElem(eFst))*sin(acq.tx.angle(iTx)) + proc.das.zGrid'*cos(acq.tx.angle(iTx));
+% 
+%                             r1      = (proc.das.xGrid-xElem(   1))*cos(acq.tx.angle(iTx)) - proc.das.zGrid'*sin(acq.tx.angle(iTx));
+%                             r2      = (proc.das.xGrid-xElem( end))*cos(acq.tx.angle(iTx)) - proc.das.zGrid'*sin(acq.tx.angle(iTx));
+%                             txApod	= double(r1 >= 0 & r2 <= 0);
+% 
+%                     end
     
-    rfRx	= zeros(zSize,xSize,nRx);
-    wghRx	= zeros(zSize,xSize,nRx);
-    for iRx=1:nRx
-        % calculate rx delays and apodization
-        rxDist	= sqrt((proc.das.xGrid-xElem(iRx)).^2 + proc.das.zGrid'.^2);
-        fNum	=  abs((proc.das.xGrid-xElem(iRx))./proc.das.zGrid');
-        rxApod	= fNum < 0.5;
-        
-        % calculate total delays
-        delTot	= (txDist + rxDist)/proc.speedOfSound + initDel;	% [s]
-        
-        % calculate sample numbers to be used in reconstruction (out-of-range sample numbers -> nSamp+1 -> sample=0)
-        iSamp	= delTot*fs + 1;                 % [samp]
-        iSamp(iSamp<1 | iSamp>nSamp-1) = nSamp;
-        
-        % calculate the rf samples (interpolated) and apodization weights
-        rfRawLine       = rfRaw(:,iRx,iTx);
-        rfRx(:,:,iRx)	= rfRawLine(floor(iSamp)).*(1-mod(iSamp,1)) ...
-                            + rfRawLine( ceil(iSamp)).*(  mod(iSamp,1));
-        wghRx(:,:,iRx)	= txApod.*rxApod;
-        
-        % modulate if iq signal is used
-        if proc.ddc.iqEnable
-            rfRx(:,:,iRx)	= rfRx(:,:,iRx).*exp(1i*2*pi*acq.tx.frequency*delTot);
-        end
+    % calculate total delays
+    delTot	= (txDist(:,:,iTx) + rxDist)/acq.speedOfSound + initDel;	% [s]
+    
+    % calculate sample numbers to be used in reconstruction (out-of-range sample numbers -> nSamp+1 -> sample=0)
+    iSamp	= delTot*fs + 1;                 % [samp]
+    
+    % calculate the rf samples (interpolated) and apodization weights
+    rfRx	= interp2(rfRaw(:,:,iTx),iRx,iSamp,'linear',0);
+    wghRx	= txApod(:,:,iTx).*rxApod;
+    
+    % modulate if iq signal is used
+    if proc.ddc.iqEnable
+        rfRx	= rfRx.*exp(1i*2*pi*acq.tx.frequency*delTot);
     end
     
     % calculate rf and weights for single tx
--- matlab/bmode/reconstructRfLin.m
+++ matlab/bmode/reconstructRfLin.m
@@ -6,9 +6,10 @@ function[rfBfr] = reconstructRfLin(rfRaw,sys,acq,proc,txDelCent)
 % rfBfr         - (nSamp,nTx)  output beamformed rf
 % 
 % Inputs:
-% rfRaw         - (nSamp,nRx,nTx) raw rf data; tx & rx apertures must be centered at the intersection of imaging line
-%               & probe surface; tx time delay of the tx aperture center element (txCentDel) must be constant for all tx's;
-%               if rfRaw is gpuArray then calculations are done on GPU;
+% rfRaw         - (nSamp,nRx,nTx) raw rf data;
+%                   tx & rx apertures must be centered at the intersection of imaging line & probe surface;
+%                   tx time delay of the tx aperture center element (txCentDel) must be constant for all tx's;
+%                   if rfRaw is gpuArray then calculations are done on GPU;
 % 
 % sys                       - system-related parameters
 % sys.nElements             - [elem] number of probe's elements
@@ -30,55 +31,51 @@ function[rfBfr] = reconstructRfLin(rfRaw,sys,acq,proc,txDelCent)
 
 %% Reconstruction
 [nSamp,nRx,nTx]	= size(rfRaw);
+rfRaw       = reshape(rfRaw,[nSamp*nRx,nTx]);
 
 fs          = acq.rx.samplingFrequency/proc.ddc.decimation;
 
 dT          = txDelCent + acq.tx.nPeriods/(2*acq.tx.frequency);	% [s] (1,1) total delay correction
 
-rVec        = (0:(nSamp-1))'/fs * acq.speedOfSound/2;           % [mm] (nSamp,1) radial distance from the line origin
-xVec        = rVec*sin(acq.tx.angle);                           % [mm] (nSamp,1) horiz. distance from the line origin
-zVec        = rVec*cos(acq.tx.angle);                           % [mm] (nSamp,1) vert.  distance from the line origin
-eVec        = (-(nRx-1)/2:(nRx-1)/2)*sys.pitch;                 % [mm] (1,nElem) horiz. position of the rx aperture elements (rel. to the line origin)
+rVec        = (0:(nSamp-1))'/fs * acq.speedOfSound/2;       % [mm] (nSamp,1) radial distance from the line origin
+xVec        = rVec*sin(acq.tx.angle);                       % [mm] (nSamp,1) horiz. distance from the line origin
+zVec        = rVec*cos(acq.tx.angle);                       % [mm] (nSamp,1) vert.  distance from the line origin
+eVec        = (-(nRx-1)/2:(nRx-1)/2)*sys.pitch;             % [mm] (1,nElem) horiz. position of the rx aperture elements
 % warning - different aperture position in rf simulation and reconstruction
 
-txDist      = rVec;                                                         % [mm] (nSamp,1) tx distance (from the line origin to each point of the imaging line)
-rxDist      = sqrt((xVec-eVec).^2 + zVec.^2);                               % [mm] (nSamp,nRx) rx distance (from each point of the imaging line to each element of the tx aperture)
+txDist      = rVec;                                         % [mm] (nSamp,1) tx distance (from the line origin)
+rxDist      = sqrt((xVec-eVec).^2 + zVec.^2);               % [mm] (nSamp,nRx) rx distance (to each rx element)
 
-rfRaw(end,:,:)	= 0;                                                        % zero the last sample to efficiently skip out-of-range samples in later processing
+t           = (txDist + rxDist)/acq.speedOfSound + dT;      % [s] (nSamp,nRx) total tx-rx time delays
+if isa(rfRaw,'gpuArray')
+    t       = gpuArray(t);
+end
 
-t           = (txDist + rxDist)/acq.speedOfSound + dT;                               % [s] (nSamp,nRx) total tx-rx time delays
+spl         = t*fs + 1;                                     % [samp] (nSamp,nRx) sample numbers
+spl(spl<1 | spl>nSamp-1) = inf;
+spl         = reshape(spl + (0:(nRx-1))*nSamp,[],1);        % [samp] (nSamp*nRx,1)
 
-spl         = t*fs + 1;                                                 % [samp] (nSamp,nRx) sample numbers to be used in image line reconstruction
-spl(spl<1 | spl>nSamp-1) = nSamp;
-
-
-splPrev     = floor(spl);                                                   % [samp] (nSamp,nRx) previous sample numbers (for interpolation, if the spl isn't integer)
-splNext     = ceil(spl);                                                    % [samp] (nSamp,nRx) next sample numbers (for interpolation, if the spl isn't integer)
-
-fNum        = abs(xVec-eVec)./zVec;
-rxApod      = fNum < 0.5;
-rxApod      = permute(rxApod./sum(rxApod,2),[1 3 2]);                       % [] (nSamp,1,nRx) normalized & reorganized rx apodization vector
+rxFNum      = abs(xVec-eVec)./zVec;
+rxApod      = rxFNum < 0.5;
+rxApod      = rxApod./sum(rxApod,2);                        % [] (nSamp,nRx) normalized rx apodization vector
 % warning - does the apodization takes into account for the clipped aperture?
 
-% % Delay & Sum
-rfRaw	= reshape(rfRaw,[nSamp*nRx,nTx]);
-splPrev	= reshape(splPrev + (0:(nRx-1))*nSamp,[],1);
-splNext	= reshape(splNext + (0:(nRx-1))*nSamp,[],1);
-spl     = spl(:);
-rfBfr	= rfRaw(splPrev,:).*(1-mod(spl,1)) ...
-        + rfRaw(splNext,:).*   mod(spl,1);
-rfBfr	= permute(reshape(rfBfr,[nSamp,nRx,nTx]),[1 3 2]);
+% Delay & Sum
+rfBfr	= interp1(1:(nSamp*nRx),rfRaw,spl,'linear',0);      % WARNING -> see the comment at the end of the script
+rfBfr	= reshape(rfBfr,[nSamp,nRx,nTx]);
 
 % modulate if iq signal is used
 if proc.ddc.iqEnable
-    rfBfr	= rfBfr.*exp(1i*2*pi*acq.tx.frequency*permute(t,[1 3 2]));
+    rfBfr	= rfBfr.*exp(1i*2*pi*acq.tx.frequency*t);
 end
 
-rfBfr = sum(rfBfr.*rxApod,3);
+rfBfr = reshape(sum(rfBfr.*rxApod,2),[nSamp,nTx]);
 
-% Optimization:
-% weighting by modulo - slower by ~10ms (470->480ms) but code is nicer than splNext-spl or spl-splPrev;
-% permute(rfRaw,[1 3 2]) and avoid squeezeng - cost of 'permute' is higher than gain from avoiding 'squeeze';
+% WARNING
+% The first argument in interp1 function is optional, code could be: rfBfr	= interp1(rfRaw,spl,'linear',0);
+% However, if interp1 is executed on GPU (rfRaw is gpuArray), then interp1 contains a bug which results in CUDA error.
+% The solution is to keep the first argument of the interp1 function.
+% Solution found here: https://uk.mathworks.com/matlabcentral/answers/462545-interp1-gpuarray-bug
 
 end
 
