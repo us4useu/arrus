e2c8c0bde1497f04e6b2425f4a9524eb665f0960
Piotr Jarosik <jarosik.piotr@gmail.com>
2020-03-06 10:23:21 +0100

    Added helper function for cufft errors.

--- python/volumetric/helper.h
+++ python/volumetric/helper.h
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <cuda_runtime.h>
+#include <cufft.h>
 
 // Based on CUDA examples common code.
 template <typename T>
@@ -9,6 +10,15 @@ void check(T result, const char *const file, const int line) {
         fprintf(stderr, "CUDA error at %s:%d, error %s (code %d): %s\n",
                 file, line, cudaGetErrorName(result), result, 
                 cudaGetErrorString(result));
+        exit(EXIT_FAILURE);
+    }
+}
+
+template <>
+void check(cufftResult result, const char *const file, const int line) {
+    if(result != CUFFT_SUCCESS) {
+        fprintf(stderr, "CUFFT error at %s:%d, error code %d\n",
+                file, line, result);
         exit(EXIT_FAILURE);
     }
 }
