8503847bec0ddf3e77339f428f4efcd46d5f5aa8
Piotr Jarosik <jarosik.piotr@gmail.com>
2020-03-10 13:40:51 +0100

    Added interpolation in complex domain.

--- python/volumetric/hsdi.cu
+++ python/volumetric/hsdi.cu
@@ -18,17 +18,30 @@ std::array<dtype, DATA_SIZE> inputBuffer;
 
 const unsigned PADDED_OX = NCHANNELS_OX*2;
 const unsigned PADDED_OY = NCHANNELS_OY*2;
-const unsigned PADDED_DATA_SIZE = NEVENTS*PADDED_OX*PADDED_OY*NSAMPLES;
-std::array<dtype, PADDED_DATA_SIZE> outputBuffer;
+const unsigned NDEPTH = 1024;
+const unsigned PADDED_DATA_SIZE = NEVENTS*PADDED_OX*PADDED_OY*NDEPTH;
+std::array<complexType, PADDED_DATA_SIZE> outputBuffer;
 
 int main(int argc, char* argv[])
 {
     typedef double realType;
     typedef cufftDoubleComplex complexType;
     // Read data
-    realType *devInBuffer, *devProcBuffer, *devOutputBuffer;
-    complexType *fftBuffer;
+    realType *devInBuffer, *devProcBuffer;
+    complexType *fftBuffer, *devOutputBuffer;
     cufftHandle fftPlanFwd, fftPlanInv;
+
+    // Precomputed parameters.
+    const dtype SPEED_OF_SOUND = 1540;
+    const dtype SAMPLING_FREQ = 25e6;
+    const dtype PITCH = 0.3e-3;
+    const dtype DF = SAMPLING_FREQ/NSAMPLES;
+    // Porzadek jest zgodny z numpy (0, ... n/2-1, -n/2 .itd)
+    const dtype DKX = 2*M_PI/(PADDED_OX*PITCH);
+    const dtype DKY = 2*M_PI/(PADDED_OY*PITCH);
+    const dtype F_MAX = SAMPLING_FREQ/2.0;
+    const dtype KZ_MAX = 2*M_PI*F_MAX/SPEED_OF_SOUND;
+    const dtype DKZ = KZ_MAX/(NDEPTH-1);
 
     std::ifstream input{"data.bin", std::ios::binary};
     input.read((char*)(inputBuffer.data()), inputBuffer.size()*sizeof(dtype));
@@ -38,11 +51,11 @@ int main(int argc, char* argv[])
     checkCudaErrors(cudaMalloc(&fftBuffer,
                                (PADDED_DATA_SIZE/2+1)*sizeof(complexType)));
     checkCudaErrors(cudaMalloc(&devOutputBuffer,
-                               (PADDED_DATA_SIZE)*sizeof(realType)));
+                               (PADDED_DATA_SIZE)*sizeof(complexType)));
     checkCudaErrors(cufftPlan3d(&fftPlanFwd, PADDED_OX, PADDED_OY,
                                 NSAMPLES, CUFFT_D2Z));
     checkCudaErrors(cufftPlan3d(&fftPlanInv, PADDED_OX, PADDED_OY,
-                                NSAMPLES, CUFFT_Z2D));
+                                NSAMPLES, CUFFT_Z2Z));
 
 
     // Pad with zeros.
@@ -59,25 +72,31 @@ int main(int argc, char* argv[])
     checkCudaErrors(cufftExecD2Z(fftPlanFwd, devProcBuffer, fftBuffer));
 
     // Interpolation & weighting
+    interpWeight<<<grid, threads>>>(devOutputBuffer, fftBuffer,
+                 PADDED_OX, PADDED_OY, NDEPTH,
+                 PADDED_OX, PADDED_OY, NSAMPLES/2+1, // R2C was used
+                 SPEED_OF_SOUND,
+                 DKX, DKY, DKZ,
+                 DF);
 
     // IFFT
-    checkCudaErrors(cufftExecZ2D(fftPlanInv, fftBuffer, devOutputBuffer));
+    checkCudaErrors(cufftExecZ2Z(fftPlanInv, fftBuffer, devOutputBuffer, CUFFT_INVERSE));
 
-    // Abs, norm?
+    // TODO(pjarosik) unpadd?
+
+    // TODO(pjarosik) Abs, norm?
 
     // Write output to a file.
-    std::cout << "Producing the output, size: "
-              << PADDED_DATA_SIZE*sizeof(dtype)
-              << std::endl;
     std::ofstream output{"pdata.bin", std::ios::binary};
 
-    checkCudaErrors(cudaMemcpy(outputBuffer.data(), devProcBuffer,
-                               PADDED_DATA_SIZE*sizeof(dtype),
+    checkCudaErrors(cudaMemcpy(outputBuffer.data(), devOutputBuffer,
+                               PADDED_DATA_SIZE*sizeof(complexType),
                                cudaMemcpyDeviceToHost));
 
-    output.write((char*)(outputBuffer.data()), outputBuffer.size()*sizeof(dtype));
+    output.write((char*)(outputBuffer.data()), outputBuffer.size()*sizeof(complexType));
     cudaFree(devInBuffer);
     cudaFree(devProcBuffer);
+    cudaFree(devOutputBuffer);
     cufftDestroy(fftPlanFwd);
     cufftDestroy(fftPlanInv);
     return 0;
--- python/volumetric/hsdi.cuh
+++ python/volumetric/hsdi.cuh
@@ -1,13 +1,16 @@
 #include <cstdio>
+#include <math.h>
+#include <cufft.h>
 
 typedef double dtype;
+typedef cufftDoubleComplex complexType;
 
 inline unsigned divup(unsigned x, unsigned div) {
     return (x + div-1) / div;
 }
 
 
-__global__ void padHalfWithZeros(double* dst, double* src,
+__global__ void padHalfWithZeros(dtype* dst, dtype* src,
                                  int dstW, int dstH,
                                  int srcW, int srcH, int depth)
 {
@@ -32,6 +35,49 @@ __global__ void padHalfWithZeros(double* dst, double* src,
         }
         else {
             dst[dstIdx] = 0.0;
+        }
+    }
+}
+
+// :param dkz: a change in a single dkz axis
+__global__ void interpWeight(complexType* dst, complexType* src,
+                             int dstW, int dstH, int dstD,
+                             int srcW, int srcH, int srcD,
+                             dtype speedOfSound,
+                             dtype dkx, dtype dky, dtype dkz,
+                             dtype df)
+{
+    const int x = blockDim.x*blockIdx.x + threadIdx.x;
+    const int y = blockDim.y*blockIdx.y + threadIdx.y;
+    const int z = blockDim.z*blockIdx.z + threadIdx.z;
+    const int halfW = srcW/2;
+    const int halfH = srcH/2;
+    const int dstIdx = z + y*dstD + x*dstD*dstH;
+
+    if(x < dstW && y < dstH && z < dstD) {
+        if(z == 0) {
+            dst[dstIdx] = {0.0, 0.0};
+        }
+        else {
+            // TODO(pjarosik) consider moving to shared memory
+            dtype kx = dkx*(-halfW+(x+halfW)%srcW);
+            dtype ky = dky*(-halfH+(y+halfH)%srcH);
+            dtype kz = dkz*z;
+
+            dtype sample = speedOfSound/(4*M_PI)*((kz*kz+kx*kx+ky*ky)/kz);
+            // Sample at df should be equal one (just cast frequencies to
+            // an index numbers).
+            sample = sample/df;
+            dtype w = speedOfSound/(4*M_PI)*((kz*kz-kx*kx-ky*ky)/kz);
+
+            // linear interp.
+            int z1 = int(sample);
+            int z2 = z1+1;
+            dtype alpha = sample-z1;
+            complexType y1 = src[z1*y*dstD+x*dstD*dstH];
+            complexType y2 = src[z2*y*dstD+x*dstD*dstH];
+            dst[dstIdx] = {w*((1-alpha)*y1.x+alpha*y2.x),
+                           w*((1-alpha)*y1.y+alpha*y2.y)};
         }
     }
 }
