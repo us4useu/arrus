6e427e0078b6fdfff763132b0741299a4ccd9508
PiotrKarwat <55211251+PiotrKarwat@users.noreply.github.com>
2020-04-28 12:43:48 +0200

    STA example added to Us4R_control

--- api/matlab/arrus/STASequence.m
+++ api/matlab/arrus/STASequence.m
@@ -1,4 +1,13 @@
 classdef STASequence < SimpleTxRxSequence
     % Synthetic Transmit Aperture Imaging sequenece.
+    
+    
+    
+    methods
+        function obj = STASequence(varargin)
+            obj = obj@SimpleTxRxSequence(varargin{:});
+        end
+        
+    end
 end
 
