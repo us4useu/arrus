1c51122412a85971d5b015607b29f42e41aa8c47
zkLog <ziemowit.klimonda@us4us.eu>
2020-05-05 18:41:51 +0200

    Tests for calculate_tx_delays().

--- api/python/arrus/utils/tests/imaging_test.py
+++ api/python/arrus/utils/tests/imaging_test.py
@@ -3,7 +3,7 @@ import numpy as np
 import scipy.io
 import arrus.tests.tools as tools
 import arrus.utils.parameters
-from arrus.utils.imaging import reconstruct_rf_img
+from arrus.utils.imaging import reconstruct_rf_img, compute_tx_delays
 
 class ReconstructRfImgEdgeCaseTest(unittest.TestCase):
     """
@@ -68,7 +68,7 @@ class ReconstructRfImgSimulatedDataTest(unittest.TestCase):
         # Given:
         file_path = tools.get_dataset_path("bmode/rfPwi_field_v2.mat")
         rf, sys_parameters, acq_parameters = \
-            arius.utils.parameters.load_matlab_file(file_path)
+            arrus.utils.parameters.load_matlab_file(file_path)
 
         desired = tools.get_dataset_path("bmode/rfPwi_field_v2_desired.mat")
         desired = scipy.io.loadmat(desired)['img']
@@ -91,6 +91,118 @@ class ReconstructRfImgSimulatedDataTest(unittest.TestCase):
 
         # Should be:
         np.testing.assert_equal(actual=actual, desired=desired)
+
+
+class ComputeTxDelaysEdgeCaseTest(unittest.TestCase):
+    """
+    Tests edge cases for computing delays function.
+    """
+# (angles, focus, pitch, c=1490, n_chanels=128
+    def setUp(self):
+        # Default parameter values.
+        self.angles = [-5, 0, 5]
+        self.focus = 50e-3
+        self.pitch = 0.3e-3
+        self.c = 1490
+        self.n_chanels = 128
+
+    def test_single_plane_wave(self):
+        # Given:
+        focus = []
+        angles = 0
+
+        actual = compute_tx_delays(
+            angles,
+            focus,
+            self.pitch,
+            self.c,
+            self.n_chanels
+        )
+
+        # Should be:
+        # We expect an array of all zeros (no input signal, no output signal).
+        np.testing.assert_equal(
+            actual=actual,
+            desired=np.zeros((1, 128), dtype=np.int16)
+        )
+
+    def test_positive_focus(self):
+        # Given:
+        focus = 1
+        angles = 0
+        pitch = 1
+        c = 1,
+        n_chan = 3
+
+        actual = compute_tx_delays(
+            angles,
+            focus,
+            pitch,
+            c,
+            n_chan
+        )
+
+        # Should be:
+        # We expect an array of all zeros (no input signal, no output signal).
+        np.testing.assert_almost_equal(
+            actual=actual,
+            desired=np.array([[0, np.sqrt(2)-1, 0]]),
+            decimal=15
+        )
+
+
+    def test_negative_focus(self):
+        # Given:
+        focus = -1
+        angles = 0
+        pitch = 1
+        c = 1,
+        n_chan = 3
+
+        actual = compute_tx_delays(
+            angles,
+            focus,
+            pitch,
+            c,
+            n_chan
+        )
+
+        # Should be:
+        # We expect an array of all zeros (no input signal, no output signal).
+        np.testing.assert_almost_equal(
+            actual=actual,
+            desired=np.array([[np.sqrt(2)-1, 0, np.sqrt(2)-1]]),
+            decimal=15
+        )
+
+    def test_triple_plane_wave(self):
+        # Given:
+        focus = []
+        angles = [-5, 0, 5]
+        pitch = self.pitch
+        c = self.c
+        nchan = self.n_chanels
+
+        actual = compute_tx_delays(
+            angles,
+            focus,
+            self.pitch,
+            self.c,
+            nchan
+        )
+
+        desired = np.empty([3, nchan])
+        desired[0, :] = np.linspace(-(nchan-1), 0, nchan)*pitch/c*np.sin(angles[0])
+        desired[1, :] = np.zeros((1, nchan))
+        desired[2, :] = np.linspace(0, (nchan-1), nchan)*pitch/c*np.sin(angles[2])
+
+        # Should be:
+        # We expect an array of all zeros (no input signal, no output signal).
+        np.testing.assert_almost_equal(
+            actual=actual,
+            desired=desired,
+            decimal=15
+        )
 
 
 if __name__ == "__main__":
