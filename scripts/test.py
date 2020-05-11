4488bc738b134f00d5b37cae47cb819533dfe591
Piotr Jarosik <piotr.jarosik@us4us.eu>
2020-05-11 20:13:23 +0200

    Ref 51: Created CMake configuration for the python unit tests. (#58)
    * Implemented cmake target for python unit tests.
    
    - enabled cmake tests
    - updated cmake module for arius package installation
    - fixed/disabled some of the currently available tests
    
    * Fixed Cmake configuration for the test files.
    
    Previous merge fix.
    
    * Added source and install dirs for cmake configuration.
    
    * Renamed directory with us4r libs dir.
    
    * Separated build and install steps.
    
    * Added run targets for cmake wrapper configuration.
    
    * Fixed typo.
    
    * Fixed tests configuration.

--- scripts/test.py
+++ scripts/test.py
@@ -1,29 +1,23 @@
 import argparse
 import os
 import subprocess
-import shutil
-
-COLOR_ERROR = '\033[91m'
-COLOR_END = '\033[0m'
 
 SRC_ENVIRON = "ARRUS_SRC_PATH"
-INSTALL_ENVIRON = "ARRUS_INSTALL_PATH"
+
 
 def assert_no_error(return_code):
     if return_code != 0:
-        print(COLOR_ERROR + "Failed building targets." + COLOR_END)
+        print("Failed testing targets.")
         exit(1)
 
+
 def main():
-    parser = argparse.ArgumentParser(description="Configures build system.")
-    parser.add_argument("--config", dest="config",
-                        type=str, required=False, default="Release")
+    parser = argparse.ArgumentParser(description="Tests the system.")
     parser.add_argument("--source_dir", dest="source_dir",
                         type=str, required=False,
                         default=os.environ.get(SRC_ENVIRON, None))
 
     args = parser.parse_args()
-    configuration = args.config
     src_dir = args.source_dir
 
     if src_dir is None:
@@ -36,7 +30,7 @@ def main():
     cmake_cmd = [
         "cmake",
         "--build", build_dir,
-        "--config", configuration
+        "--target", "test"
     ]
 
     print("Calling: %s" % (" ".join(cmake_cmd)))
