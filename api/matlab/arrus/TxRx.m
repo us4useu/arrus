c3af82e09d3c3bc055d0cf74ab2c7f20a40853a8
zkLog <ziemowit.klimonda@us4us.eu>
2020-06-19 12:51:20 +0200

    TxRx class added.

--- api/matlab/arrus/TxRx.m
+++ api/matlab/arrus/TxRx.m
@@ -1,17 +1,27 @@
 classdef TxRx
-    % class corresponding to single atomic 'transmit-and-receive' event
+    % class corresponding to single 'transmit-and-receive' event
     
     properties
-        Tx
-        Rx
+        Tx@Tx = Tx()
+        Rx@Rx = Rx()
     end
     
     
     methods
-        function obj = TxRx()
-            
-            
+        function obj = TxRx(varargin)
+            if nargin ~= 0
+                p = inputParser;
+                                
+                % adding parameters to parser
+                addParameter(p, 'Tx',Tx())
+                addParameter(p, 'Rx',Rx())
+                parse(p,varargin{:})
+                                
+                obj.Tx = p.Results.Tx;
+                obj.Rx = p.Results.Rx;
+            end                 
         end
+        
+        
     end
-    
 end
