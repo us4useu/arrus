Fixing tx_delays computation.

api/python/arrus/kernels/imaging.py
@@ -3,6 +3,7 @@ import arrus.exceptions
 from arrus.ops.us4r import (
     Tx, Rx, TxRx, TxRxSequence, Pulse
 )
+import arrus.utils.imaging
 
 
 def create_lin_sequence(context):
@@ -16,6 +17,7 @@ def create_lin_sequence(context):
     pitch = context.device.probe.model.pitch
     # sequence parameters
     op = context.op
+
     n_elem_sub = op.tx_aperture_size
     focal_depth = op.tx_focus
     sample_range = op.rx_sample_range
@@ -54,9 +56,6 @@ def create_lin_sequence(context):
     else:
         focus = [0, focal_depth]
 
-    # enumerate delays mask and padding for each tx/rx event
-    subaperture_delays = enum_classic_delays(n_elem_sub, pitch, c, focus)
-
     def get_ap(center_element, size):
         left_half_size = (size-1)//2  # e.g. size 32 -> 15, size 33 -> 16
         right_half_size = size//2  # e.g. size 32 -> 16, size 33 -> 16
@@ -72,6 +71,16 @@ def create_lin_sequence(context):
         aperture = np.zeros((n_elem, ), dtype=np.bool)
         aperture[actual_origin:(actual_end+1)] = True
         return aperture, (left_padding, right_padding)
+
+    tx_apertures = []
+    for tx_center_element in tx_centers:
+        tx_apertures.append(get_ap(tx_center_element, tx_ap_size))
+
+    tx_delays = compute_tx_delays(op, context.device.probe.model, c)
+    for
+    txrxs = []
+
+    # use tx apertures to compute tx delays
 
     # create tx/rx objects list
     def create_tx_rx(tx_center_element, rx_center_element):
@@ -91,6 +100,59 @@ def create_lin_sequence(context):
         return TxRx(tx, rx, pri)
     txrxlist = [create_tx_rx(*c) for c in zip(tx_centers, rx_centers)]
     return TxRxSequence(txrxlist, tgc_curve=tgc_curve)
+
+
+def get_tx_aperture_center_coords(sequence, probe):
+    n_elements = probe.n_elements
+    pitch = probe.pitch
+    curvature_radius = probe.curvature_radius
+    tx_aperture_center_element = sequence.tx_aperture_center_element
+
+    element_position = np.arange(-(n_elements - 1) / 2,
+                                 (n_elements - 1) / 2)*pitch
+
+    if not probe.is_convex_array():
+        angle = np.zeros(n_elements)
+    else:
+        angle = element_position / curvature_radius
+
+    tx_aperture_center_angle = np.interp(tx_aperture_center_element,
+                                         np.arange(1, n_elements), angle)
+    tx_aperture_center_z = np.interp(tx_aperture_center_element,
+                                     np.arange(1, n_elements),
+                                     probe.element_pos_z)
+    tx_aperture_center_x = np.interp(tx_aperture_center_element,
+                                     np.arange(1, n_elements),
+                                     probe.element_pos_x)
+
+    return tx_aperture_center_angle, tx_aperture_center_x, tx_aperture_center_z
+
+
+def compute_tx_delays(sequence, probe, speed_of_sound):
+    element_x, element_z = probe.element_pos_x, probe.element_pos_z
+    element_x, element_z = np.atleast_2d(element_x), np.atleast_2d(element_z)
+
+    tx_center_angle, tx_center_x, tx_center_z = get_tx_aperture_center_coords(
+        sequence, probe)
+    tx_center_angle = np.atleast_2d(tx_center_angle)
+    tx_center_x = np.atleast_2d(tx_center_x)
+    tx_center_z = np.atleast_2d(tx_center_z)
+
+    tx_angle = 0
+    tx_focus = sequence.tx_focus
+    tx_angle_cartesian = tx_center_angle + tx_angle
+
+    focus_x = tx_center_x + tx_focus*np.sin(tx_angle_cartesian)
+    focus_z = tx_center_z + tx_focus*np.cos(tx_angle_cartesian)
+
+    # (n_elements, n_tx)
+    tx_delays = np.sqrt((focus_x - element_x.T)**2 + (focus_z-element_z.T)**2) / speed_of_sound
+    tx_delays_center = np.sqrt((focus_x - tx_center_x.T)**2 + (focus_z-tx_center_z.T)**2) / speed_of_sound
+
+    foc_defoc = 1 - 2*float(tx_focus > 0)
+    tx_delays = tx_delays*foc_defoc
+    tx_delays_center = tx_delays_center*foc_defoc
+
 
 
 def enum_classic_delays(n_elem, pitch, c, focus):
@@ -114,9 +176,14 @@ def enum_classic_delays(n_elem, pitch, c, focus):
         raise ValueError("Bad focus - should be scalar, 1-dimensional ndarray, "
                          "or 2-dimensional ndarray")
 
+
     aperture_width = (n_elem-1)*pitch
     el_coord_x = np.linspace(-aperture_width/2, aperture_width/2, n_elem)
     element2focus_distance = np.sqrt((el_coord_x - xf)**2 + zf**2)
+
+    distance = np.sqrt()
+
     dist_max = np.amax(element2focus_distance)
     delays = (dist_max - element2focus_distance)/c
     return delays
+

api/python/arrus/utils/imaging.py
@@ -230,6 +230,7 @@ class RxBeamforming:
         context = metadata.context
         probe_model = metadata.context.device.probe.model
         seq = metadata.context.sequence
+        raw_seq = metadata.context.raw_sequence
         medium = metadata.context.medium
 
         n_tx, n_rx, n_samples = data.shape
@@ -248,10 +249,7 @@ class RxBeamforming:
                                     dtype=data.dtype)
 
         # -- Delays
-        start_sample = context.custom_data["start_sample"] + 1
-        tx_delay_center = context.custom_data["tx_delay_center"]
-        rx_aperture_origin = context.custom_data["rx_aperture_origin"]
-        # TODO(pjarosik) Make sure that we use echo data.
+
         acq_fs = (metadata.context.device.sampling_frequency
                   / seq.downsampling_factor)
         fs = metadata.data_description.sampling_frequency
@@ -262,6 +260,17 @@ class RxBeamforming:
         else:
             c = medium.speed_of_sound
         tx_angle = 0 # TODO use appropriate tx angle
+
+        tx_delay_center = _get_tx_delay_center(seq, probe_model, c)
+        tx_delay_center = context.custom_data["tx_delay_center"]
+        # print(f"tx delay center: matlab: {tx_delay_center_mat}, python: {tx_delay_center}")
+        # Assuming, that all tx/rxs have the constant start sample value.
+        start_sample = 0 # raw_seq.ops[0].rx.sample_range[0] TODO
+        start_sample_mat = context.custom_data["start_sample"] + 1
+        print(f"start sample: mat {start_sample_mat}, python: {start_sample} ")
+        rx_aperture_origin = _get_rx_aperture_origin(seq)
+        rx_aperture_origin_mat = context.custom_data["rx_aperture_origin"]
+        print(f"rx aperture origin: {rx_aperture_origin_mat}, python: {rx_aperture_origin}")
 
         burst_factor = n_periods / (2 * fc)
         initial_delay = (- start_sample / acq_fs
@@ -403,6 +412,7 @@ class ScanConversion:
         probe = metadata.context.device.probe.model
         medium = metadata.context.medium
         data_desc = metadata.data_description
+        raw_seq = metadata.context.raw_sequence
 
         if not probe.is_convex_array():
             raise ValueError(
@@ -411,10 +421,13 @@ class ScanConversion:
         n_samples, _ = data.shape
         seq = metadata.context.sequence
         custom_data = metadata.context.custom_data
-        start_sample = custom_data["start_sample"]
+        # start_sample = custom_data["start_sample"]
+        start_sample = 0# raw_seq.ops[0].rx.start_sample[0]
         fs = data_desc.sampling_frequency
         c = medium.speed_of_sound
-        tx_ap_cent_ang = custom_data["tx_aperture_center_angle"]
+        tx_ap_cent_ang, _, _ = _get_tx_aperture_center_coords(seq, probe)
+        tx_ap_cent_ang_mat = custom_data["tx_aperture_center_angle"]
+        print(f"tx ap cent ang: matlab: {tx_ap_cent_ang_mat}, python: {tx_ap_cent_ang}")
 
         z_grid_moved = self.z_grid.T + probe.curvature_radius - np.max(
             probe.element_pos_z)
@@ -464,3 +477,32 @@ class LogCompression:
             data = data.get()
         data[data == 0] = 1e-9
         return 20 * np.log10(data), metadata
+
+
+
+
+def _get_rx_aperture_origin(sequence):
+    rx_aperture_size = sequence.rx_aperture_size
+    rx_aperture_center_element = sequence.rx_aperture_center_element
+    rx_aperture_origin = np.round(rx_aperture_center_element -
+                               (rx_aperture_size - 1) / 2 + 1e-9)
+    return rx_aperture_origin
+
+
+def _get_tx_delay_center(sequence, probe, speed_of_sound):
+    tx_angle = 0  # TODO implement custom tx angle
+    tx_aperture_center_angle, tx_aperture_center_x, tx_aperture_center_z = \
+        _get_tx_aperture_center_coords(sequence, probe)
+
+    x_focus = tx_aperture_center_x + sequence.tx_focus * np.sin(tx_angle)
+    z_focus = tx_aperture_center_z + sequence.tx_focus * np.cos(tx_angle)
+
+    print("Printy")
+    print(tx_aperture_center_x)
+    print(tx_aperture_center_z)
+
+    tx_delay_center = np.sqrt((x_focus - tx_aperture_center_x)**2
+                              +(z_focus-tx_aperture_center_z)**2)/speed_of_sound
+    print(tx_delay_center)
+    return np.max(tx_delay_center)
+

api/python/examples/classical_beamforming.py
@@ -23,12 +23,27 @@ arrus.set_clog_level(arrus.logging.INFO)
 arrus.add_log_file("test.log", arrus.logging.TRACE)
 
 
-def display_data(frame_number, data, metadata, imaging_pipeline):
+def init_display(aperture_size, n_samples):
     fig, ax = plt.subplots()
     fig.set_size_inches((7, 7))
-    ax.imshow(data)
-    ax.set_aspect('auto')
+    ax.set_xlabel("OX")
+    ax.set_ylabel("OZ")
+    image_w, image_h = aperture_size, n_samples
+    canvas = plt.imshow(np.zeros((image_w, image_h)),
+                        vmin=np.iinfo(np.int16).min,
+                        vmax=np.iinfo(np.int16).max, cmap="gray")
     fig.show()
+    return fig, ax, canvas
+
+
+def display_data(frame_number, data, metadata, imaging_pipeline, figure, ax, canvas):
+    # TODO use the imaging pipeline
+    i = 32
+    print(f"Displaying frame {frame_number}, scanline data: {i}")
+    canvas.set_data(data[i*4096:(i+1)*4096, :])
+    ax.set_aspect("auto")
+    figure.canvas.flush_events()
+    plt.draw()
 
 
 def display_raw_data(data):
@@ -82,27 +97,34 @@ def main():
     parser.add_argument("--n", dest="n",
                         help="How many times should the operation be performed.",
                         required=False, type=int, default=100)
+    parser.add_argument("--host_buffer_size", dest="host_buffer_size",
+                        help="Host buffer size.", required=False, type=int, default=2)
     args = parser.parse_args()
 
     seq = LinSequence(
-        tx_aperture_center_element=np.arange(7, 182),
+        tx_aperture_center_element=np.arange(1, 182),
         tx_aperture_size=64,
         tx_focus=30e-3,
         pulse=Pulse(center_frequency=5e6, n_periods=3.5, inverse=False),
-        rx_aperture_center_element=np.arange(7, 182),
+        rx_aperture_center_element=np.arange(1, 182),
         rx_aperture_size=64,
         rx_sample_range=(0, 4096),
-        pri=100e-6,
+        pri=100e-6, # pulse repetition interval, this is the time between succesive transmits on the system
+        # We 175 scanlines
+        # 175*100us, 17.5 ms
         downsampling_factor=1,
         tgc_start=14,
         tgc_slope=2e2,
         speed_of_sound=1490)
     bmode_imaging = create_bmode_imaging_pipeline()
 
+    if args.action == "img":
+        fig, ax, canvas = init_display(4096, 32)
+
     action_func = {
         "nop":  None,
         "save": save_raw_data,
-        "img":  lambda frame_number, data, metadata: display_data(frame_number, data, metadata, bmode_imaging)
+        "img":  lambda frame_number, data, metadata: display_data(frame_number, data, metadata, bmode_imaging, fig, ax, canvas)
     }[args.action]
 
     # Here starts communication with the device.
@@ -116,7 +138,8 @@ def main():
     # Set initial voltage on the us4r-lite device.
     us4r.set_hv_voltage(30)
     # Upload sequence on the us4r-lite device.
-    buffer = us4r.upload(seq, mode="sync")
+    buffer = us4r.upload(seq, mode="sync",
+                         host_buffer_size=args.host_buffer_size)
 
     # Start the device.
     us4r.start()
@@ -124,10 +147,14 @@ def main():
     arrus.logging.log(arrus.logging.INFO, f"Running {args.n} iterations.")
     for i in range(args.n):
         start = time.time()
+
         data, metadata = buffer.tail()
+
         if action_func is not None:
             action_func(i, data, metadata)
+
         buffer.release_tail()
+
         times.append(time.time()-start)
 
     arrus.logging.log(arrus.logging.INFO,