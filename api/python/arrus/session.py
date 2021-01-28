955b627a0707a71d57290102fd5af0bd3e113fac
Piotr Jarosik <piotr.jarosik@us4us.eu>
2021-01-28 09:51:40 +0100

    Implemented callback mechanism for python.

--- api/python/arrus/session.py
+++ api/python/arrus/session.py
@@ -15,6 +15,9 @@ import arrus.devices.cpu
 import arrus.devices.gpu
 import arrus.ops.us4r
 import arrus.ops.imaging
+import arrus.kernels.kernel
+import arrus.utils
+import arrus.framework
 
 
 class AbstractSession(abc.ABC):
@@ -73,10 +76,61 @@ class Session(AbstractSession):
 
     def upload(self, scheme: arrus.ops.us4r.Scheme):
         """
-        
-        :param scheme:
-        :return:
+        Uploads a given sequence of operations to perform on the device.
+
+        :param scheme: scheme to upload
+        :raises: ValueError when some of the input parameters are invalid
+        :return: a data buffer and constant metadata
         """
+        # Verify the input parameters.
+        # Prepare sequence to load
+        us_device = self.get_device("/Us4R:0")
+        us_device_dto = us_device._get_dto()
+        medium = self._context.medium
+        seq = scheme.tx_rx_sequence
+        # Device should have sampling_frequency property (DTO?)
+        kernel_context = self._create_kernel_context(seq, us_device_dto, medium)
+        raw_seq = arrus.kernels.get_kernel(type(seq))(kernel_context)
+
+        actual_scheme = arrus.utils.copy_frozen_dataclass(scheme, tx_rx_sequence=raw_seq)
+        core_scheme = arrus.utils.core.convert_to_core_scheme(actual_scheme)
+        upload_result = self._session_handle.upload(core_scheme)
+
+        # Prepare data buffer and constant context metadata
+        fcm = arrus.core.getFrameChannelMapping(upload_result)
+        buffer_handle = arrus.core.getFifoBuffer(upload_result)
+
+        ###
+        # -- Constant metadata
+        # --- FCM
+        fcm_frame, fcm_channel = arrus.utils.core.convert_fcm_to_np_arrays(fcm)
+        fcm = arrus.devices.us4r.FrameChannelMapping(
+            frames=fcm_frame, channels=fcm_channel, batch_size=1)
+
+        # --- Frame acquisition context
+        fac = self._create_frame_acquisition_context(seq, raw_seq, us_device_dto, medium)
+        echo_data_description = self._create_data_description(raw_seq, us_device_dto, fcm)
+
+        # --- Data buffer
+        n_samples = raw_seq.get_n_samples()
+
+        if len(n_samples) > 1:
+            raise arrus.exceptions.IllegalArgumentError(
+                "Currently only a sequence with constant number of samples "
+                "can be accepted.")
+        n_samples = next(iter(n_samples))
+        input_shape = self._get_physical_frame_shape(fcm, n_samples, rx_batch_size=1)
+
+        buffer = arrus.framework.DataBuffer(
+            buffer_handle=buffer_handle,
+            data_description=echo_data_description,
+            frame_shape=input_shape,
+            rx_batch_size=1)
+
+        const_metadata = arrus.metadata.ConstMetadata(
+            context=fac, data_desc=echo_data_description,
+            input_shape=input_shape, is_iq_data=False, dtype="int16")
+        return buffer, const_metadata
 
     def get_device(self, path: str):
         """
@@ -133,5 +187,29 @@ class Session(AbstractSession):
             cupy.cuda.device.Device(0).use()
             devices["/GPU:0"] = arrus.devices.gpu.GPU(0)
         return devices
+
+    def _create_kernel_context(self, seq, device, medium):
+        return arrus.kernels.kernel.KernelExecutionContext(
+            device=device, medium=medium, op=seq, custom={})
+
+    def _create_frame_acquisition_context(self, seq, raw_seq, device, medium):
+        return arrus.metadata.FrameAcquisitionContext(
+            device=device, sequence=seq, raw_sequence=raw_seq,
+            medium=medium, custom_data={})
+
+    def _create_data_description(self, raw_seq, device, fcm):
+        return arrus.metadata.EchoDataDescription(
+            sampling_frequency=device.sampling_frequency /
+                               raw_seq.ops[0].rx.downsampling_factor,
+            custom={"frame_channel_mapping": fcm}
+        )
+
+    def _get_physical_frame_shape(self, fcm, n_samples, n_channels=32,
+                                  rx_batch_size=1):
+        # TODO: We assume here, that each frame has the same number of samples!
+        # This might not be case in further improvements.
+        n_frames = np.max(fcm.frames) + 1
+        return n_frames * n_samples * rx_batch_size, n_channels
+
 
 
