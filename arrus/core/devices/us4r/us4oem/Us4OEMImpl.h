17b79b08ee626786f7c23654c50841e12934c121
Piotr Jarosik <piotr.jarosik@us4us.eu>
2021-02-15 13:04:41 +0100

    Increased minimum rx time epsilon.

--- arrus/core/devices/us4r/us4oem/Us4OEMImpl.h
+++ arrus/core/devices/us4r/us4oem/Us4OEMImpl.h
@@ -63,7 +63,7 @@ public:
     static constexpr float SAMPLING_FREQUENCY = 65e6;
     static constexpr uint32 SAMPLE_DELAY = 240;
     static constexpr float RX_DELAY = 0.0;
-    static constexpr float RX_TIME_EPSILON = 0e-6f;
+    static constexpr float RX_TIME_EPSILON = 5e-6f;
     static constexpr uint32 MIN_NSAMPLES = 64;
     static constexpr uint32 MAX_NSAMPLES = 16384;
     // Data
