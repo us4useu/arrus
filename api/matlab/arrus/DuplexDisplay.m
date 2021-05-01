f4b9ff741f18a01bb84aef148272654a586b30bd
PiotrKarwat <piotr.karwat@us4us.eu>
2021-05-01 16:50:05 +0200

    DuplexDisplay - major changes, final shape

--- api/matlab/arrus/DuplexDisplay.m
+++ api/matlab/arrus/DuplexDisplay.m
@@ -2,19 +2,30 @@ classdef DuplexDisplay < handle
 
     properties(Access = private)
         hFig
-        hAxDuplex
+        hAx
         hImg
         hQvr
         xGrid
         zGrid
+        colorEnable
+        vectorEnable
         showTimes
         dynamicRange
         powerThreshold
     end
     
     methods 
-        function obj = DuplexDisplay(xGrid, zGrid, dynamicRange, powerThreshold)
+        function obj = DuplexDisplay(proc, dynamicRange, powerThreshold, subplotEnable)
+            
+            if nargin < 4
+                subplotEnable = false;
+            end
+            
             if nargin < 3
+                powerThreshold = -inf;
+            end
+            
+            if nargin < 2
                 dynamicRange = [0 60];
             else 
                 if ~all(size(dynamicRange) == [1 2])
@@ -23,38 +34,49 @@ classdef DuplexDisplay < handle
                 end
             end
             
-            obj.xGrid = xGrid;
-            obj.zGrid = zGrid;
+            obj.xGrid = proc.xGrid;
+            obj.zGrid = proc.zGrid;
+            obj.colorEnable = proc.colorEnable;
+            obj.vectorEnable = proc.vectorEnable;
             obj.dynamicRange = dynamicRange;
             obj.powerThreshold = powerThreshold;
             
             % Create figure.
             obj.hFig = figure();
-            for iAx=1:4
-                
-                if iAx==2
-                    obj.hAxDuplex = subplot(2,2,iAx);
-                    obj.hImg(iAx) = image  (xGrid*1e3, zGrid*1e3,[]);
-                else
-                    subplot(2,2,iAx);
-                    obj.hImg(iAx) = imagesc(xGrid*1e3, zGrid*1e3,[]);
+            if subplotEnable && (obj.colorEnable || obj.vectorEnable)
+                if obj.colorEnable
+                    subplotColorMaps = cat(3, gray, gray, jet, hot);
+                    subplotDynRange = [dynamicRange; dynamicRange; [-pi pi]; dynamicRange];
+                elseif obj.vectorEnable
+                    subplotColorMaps = cat(3, gray, gray, hot, hot);
+                    subplotDynRange = [dynamicRange; dynamicRange; [0 2*pi]; dynamicRange];
                 end
                 
-                if iAx==1
-                    colormap(gca,gray);
-                    set(gca,'CLim',dynamicRange);
-                elseif iAx==3
-                    colormap(gca,hot);
-                    set(gca,'CLim',dynamicRange);
-                elseif iAx==2 || iAx==4
-                    % will require change for vector doppler
-                    colormap(gca,jet);
-                    set(gca,'CLim',[-pi pi]);
+                for iAx=1:4
+                    obj.hAx(iAx) = subplot(2,2,iAx);
+                    obj.hImg(iAx) = image(obj.xGrid*1e3, obj.zGrid*1e3, []);
+                    colormap(gca,subplotColorMaps(:,:,iAx));
+                    colorbar;
+                    xlabel('x [mm]');
+                    ylabel('z [mm]');
+                    daspect([1 1 1]);
+                    set(gca, 'CLim', subplotDynRange(iAx,:));
+                    set(gca, 'XLim', obj.xGrid([1 end])*1e3);
+                    set(gca, 'YLim', obj.zGrid([1 end])*1e3);
                 end
+            else
+                obj.hAx = axes();
+                obj.hImg = image(obj.xGrid*1e3, obj.zGrid*1e3, []);
+                colormap(gca,gray);
                 colorbar;
-                xlabel('x [mm]'), ylabel('z [mm]'), daspect([1 1 1]);
-                set(gca, 'XLim', xGrid([1 end])*1e3, 'YLim', zGrid([1 end])*1e3);
+                xlabel('x [mm]');
+                ylabel('z [mm]');
+                daspect([1 1 1]);
+                set(gca, 'CLim', dynamicRange);
+                set(gca, 'XLim', obj.xGrid([1 end])*1e3);
+                set(gca, 'YLim', obj.zGrid([1 end])*1e3);
             end
+            
             obj.hQvr = nan;
         end
         
@@ -69,67 +91,71 @@ classdef DuplexDisplay < handle
         function updateImg(obj, data)
             % Updates currently displayed image.
             %
-            % :param img: an image to display
+            % :param data: duplex data to display
             try
-                [nZPix,nXPix,~,nComp] = size(data);
+                [nZPix,nXPix,~,~] = size(data);
                 
                 bmode = data(:,:,:,1);
-                
-                if nComp==3
-                    vectorEnable = false;
-                    power = data(:,:,:,2);
-                    color = data(:,:,:,3);
-                    colorMap = jet(128);
-                    colorOffset = 0.5;
-                elseif nComp==5
-                    vectorEnable = true;
-                    power = data(:,:,:,2:3);
-                    color = data(:,:,:,4:5);
-                    colorMap = hot(128);
-                    colorOffset = 0;
-                end
-                
                 bmode(isnan(bmode)) = -inf;
-                power(isnan(power)) = -inf;
-                color(isnan(color)) = 0;
                 
-                duplexMask = (power >= obj.powerThreshold) & all(power > -inf, 4);
+                bmodeRGB = (bmode - obj.dynamicRange(1)) / diff(obj.dynamicRange);
+                bmodeRGB = max(0,min(1,bmodeRGB));
+                bmodeRGB = bmodeRGB .* ones(1,1,3);   % colormap = gray
                 
-                if vectorEnable
-                    % fixed values of txrxAng!!!
-                    txrxAng = (0 + 20*pi/180 * [-1;1]) / 2;   % [rad] (1 x 2)
-                    txrxAng	= reshape(txrxAng,1,1,1,[]);
-                    colorX	=   diff(-color.*duplexMask./cos(txrxAng),                    [],4) / diff(tan(txrxAng));
-                    colorZ	= - diff(-color.*duplexMask./cos(txrxAng).*tan(flip(txrxAng)),[],4) / diff(tan(txrxAng));
-                    color	= sqrt(colorX.^2 + colorZ.^2);
-                    power	= mean(power,4);
-                    duplexMask = color ~= 0;
+                if obj.colorEnable || obj.vectorEnable
+                    power = data(:,:,:,2);
+                    powerMap = hot(128);
+                    if obj.colorEnable
+                        color = data(:,:,:,3);
+                        colorMap = jet(128);
+                        colorOffset = 0.5;
+                    elseif obj.vectorEnable
+                        colorX = data(:,:,:,3);
+                        colorZ = data(:,:,:,4);
+                        color = sqrt(colorX.^2 + colorZ.^2);
+                        colorMap = hot(128);
+                        colorOffset = 0;
+                    end
+                    power(isnan(power)) = -inf;
+                    color(isnan(color)) = 0;
+                    
+                    colorRGB = color/2/pi + colorOffset;
+                    colorRGB = max(0,min(1,colorRGB));
+                    colorRGB = reshape(colorMap(1+round(colorRGB*127),:),nZPix,nXPix,3);
+                    
+                    powerRGB = (power - obj.dynamicRange(1)) / diff(obj.dynamicRange);
+                    powerRGB = max(0,min(1,powerRGB));
+                    powerRGB = reshape(powerMap(1+round(powerRGB*127),:),nZPix,nXPix,3);
+                    
+                    duplexMask = (power >= obj.powerThreshold);
+                    imageRGB = bmodeRGB.*~duplexMask + colorRGB.*duplexMask;
+                else
+                    imageRGB = bmodeRGB;
                 end
                 
-                duplexBMode = (bmode - obj.dynamicRange(1)) / diff(obj.dynamicRange) .* ones(1,1,3);
-                duplexBMode = max(0,min(1,duplexBMode));
-                duplexColor = reshape(colorMap(1+round(max(0,min(1, color/2/pi + colorOffset))*127),:),nZPix,nXPix,3);
                 
-                duplex = duplexBMode.*~duplexMask + duplexColor.*duplexMask;
+                set(obj.hImg(1), 'CData', imageRGB);
+                if numel(obj.hImg)==4
+                    set(obj.hImg(2), 'CData', bmodeRGB);
+                    set(obj.hImg(3), 'CData', colorRGB);
+                    set(obj.hImg(4), 'CData', powerRGB);
+                end
                 
-                set(obj.hImg(1), 'CData', bmode);
-                set(obj.hImg(2), 'CData', duplex);
-                set(obj.hImg(3), 'CData', power);
-                set(obj.hImg(4), 'CData', color);
-                
-                if vectorEnable
-                    % fixed sparsing coefficient!!!
+                if obj.vectorEnable
                     vecDec	= 20;
                     
-                    vXSel	= vecDec:vecDec:nXPix;
-                    vZSel	= vecDec:vecDec:nZPix;
+                    vXSel	= vecDec:vecDec:(nXPix-vecDec+1);
+                    vZSel	= vecDec:vecDec:(nZPix-vecDec+1);
                     vMultip	= vecDec*diff(obj.xGrid(1:2)*1e3)/pi  /2;
+                    
+                    colorX(~duplexMask) = nan;
+                    colorZ(~duplexMask) = nan;
                     
                     if ishandle(obj.hQvr)
                         delete(obj.hQvr);
                     end
                     
-                    axes(obj.hAxDuplex), hold on;
+                    axes(obj.hAx), hold on;
                     obj.hQvr = quiver(	obj.xGrid(vXSel)*1e3, ...
                                         obj.zGrid(vZSel)*1e3, ...
                                         vMultip*colorX(vZSel,vXSel), ...
