7c84bedd67e4f4d4d2e0248c0afa330754a4f8a3
Piotr Jarosik <piotr.jarosik@us4us.eu>
2021-06-04 15:34:27 +0200

    Work in progress

--- api/matlab/examples/Us4R_ATL_control_batches.m
+++ api/matlab/examples/Us4R_ATL_control_batches.m
@@ -1,7 +1,7 @@
 
 % path to the MATLAB API files
 addpath('../arrus');
-addpath('C:\Users\Public\us4oem-releases\develop\matlab\')
+addpath('C:\Users\Public\us4oem-releases\ref-US4R-21\matlab\')
 
 txFrequency = 5e6;
 samplingFrequency = 65e6;
@@ -14,7 +14,7 @@ nUs4OEM = 2;
 us	= Us4R('nUs4OEM',      nUs4OEM, ...
            'probeName',   'L7-4', ...
            'adapterType', 'atl/philips', ...
-           'voltage',      40, ...
+           'voltage',      50, ...
            'logTime',      true);
        
 seqPWI = PWISequence(	'txApertureCenter', 0*1e-3, ...
@@ -30,6 +30,8 @@ seqPWI = PWISequence(	'txApertureCenter', 0*1e-3, ...
                         'tgcStart',         14, ...
                         'tgcSlope',         2e2);
 
+xGrid = (-20:0.05:20)*1e-3;
+zGrid = (  0:0.05:40)*1e-3;
 % GPU/CPU reconstruction implemented in matlab.
 rec = Reconstruction(   'filterEnable',     true, ...
                         'filterACoeff',     filtA, ...
@@ -37,33 +39,46 @@ rec = Reconstruction(   'filterEnable',     true, ...
                         'iqEnable',         true, ...
                         'cicOrder',         2, ...
                         'decimation',       4, ...
-                        'xGrid',            (-20:0.10:20)*1e-3, ...
-                        'zGrid',            (  0:0.10:100)*1e-3);
+                        'xGrid',            (-20:0.05:20)*1e-3, ...
+                        'zGrid',            (  0:0.05:40)*1e-3);
 
                     
                     % TODO check target PRI
-us.upload(seqPWI);
+us.upload(seqPWI,rec);
 
-us.prepareBuffer(1);
-us.acquireToBuffer(1);
+us.prepareBuffer(27);
+us.acquireToBuffer(27);
 
 triggerNumbers = [];
 timestamps = [];
 
-for i=1:1
+for i=1:25
+    disp(['frame ', num2str(i)]);
     rf = us.popBufferElement();
-    frameMetadata = rf(:, 1);
+    frameMetadata = rf(1, 1:1024*17:end);
     % trigger number
-    triggerNumber = frameMetadata(1);
-    triggerNumbers = [triggerNumbers triggerNumber];
+    % disp(frameMetadata);
+    triggerNumbers = [triggerNumbers frameMetadata];
     % frame timestamp - the time when the frame was actually acquired.
-    metadataInt8 = typecast(frameMetadata, 'int8');
-    timestamp = metadataInt8(9:16); % bytes 8-16 contains timestamp
-    timestamp = double(typecast(timestamp, 'uint64'))/65e6;
-    timestamps = [timestamps timestamp];
+    % metadataInt8 = typecast(frameMetadata, 'int8');
+    % timestamp = metadataInt8(9:16); % bytes 8-16 contains timestamp
+    % timestamp = double(typecast(timestamp, 'uint64'))/65e6;
+    % timestamps = [timestamps timestamp];
     % disp(size(rf));
     % imagesc(rf);
+%     rf = reshape(permute(reshape(rf,32,1024,2,17,100,2),[2 1 6 3 4 5]),1024,128,17,100);
+%     obj.hFig = figure();
+%     obj.hImg = imagesc(xGrid*1e3, zGrid*1e3,[]);
+%     colormap(gray);
+%     colorbar; 
+%     daspect([1 1 1]);
+%     set(gca,'CLim',[20 80]);
+% 
+%     for j = 1:100
+%         img = us.reconstructOffline(rf(:,:,:,j));
+%         set(obj.hImg, 'CData', img);
+%         pause(0.1);
+%     end
 end
-
 
 
