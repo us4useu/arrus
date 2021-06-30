6e1ec4314f1ad389c1c12db7b37a8e8904b5d628
Piotr Jarosik <piotr.jarosik@us4us.eu>
2021-06-30 10:59:58 +0200

    Added __version__ attribute in ARRUS Python main module.

--- api/python/arrus/__init__.py
+++ api/python/arrus/__init__.py
@@ -5,6 +5,8 @@ import os
 os.environ["PATH"] += os.pathsep + os.path.dirname(os.path.join(os.path.abspath(__file__)))
 
 
+__version__ = "${PROJECT_VERSION}"
+
 from arrus.session import Session
 from arrus.logging import (
     set_clog_level,
