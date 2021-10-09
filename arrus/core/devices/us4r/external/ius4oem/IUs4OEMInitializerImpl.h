012f1418064fc87fb232dc6aa709a1f38001db57
Piotr Jarosik <piotr.jarosik@us4us.eu>
2021-10-09 21:53:38 +0200

    ref-US4R-40: Removed TBB dependency, added FPGATemperature getter, fixed jenkins file. (#236)

--- arrus/core/devices/us4r/external/ius4oem/IUs4OEMInitializerImpl.h
+++ arrus/core/devices/us4r/external/ius4oem/IUs4OEMInitializerImpl.h
@@ -2,9 +2,9 @@
 #define ARRUS_CORE_DEVICES_US4R_EXTERNAL_IUS4OEM_IUS4OEMINITIALIZERIMPL_H
 
 #include <algorithm>
-#include <execution>
 #include <mutex>
 #include <iostream>
+#include <future>
 
 #include "IUs4OEMInitializer.h"
 
@@ -32,30 +32,17 @@ public:
         // Us4OEMs are initialized here.
     }
 private:
-    void initializeUs4oems(std::vector<IUs4OEMHandle> &ius4oems, int level) {
-        std::vector<std::exception> exceptions;
-        std::mutex exceptions_mutex;
 
-        std::for_each(
-            std::execution::par_unseq, std::begin(ius4oems), std::end(ius4oems),
-            [&](IUs4OEMHandle &u) {
-                try {
-                    u->Initialize(level);
-                }
-                catch(const std::exception &e) {
-                    std::lock_guard guard(exceptions_mutex);
-                    exceptions.push_back(e);
-                }
-                catch(...) {
-                    std::lock_guard guard(exceptions_mutex);
-                    exceptions.push_back(std::runtime_error("Unknown exception during Us4OEM initialization."));
-                }
-            }
-        );
-        if(!exceptions.empty()) {
-            // TODO create a complete list of error messages
-            // now throw any of the exception
-            throw exceptions.front();
+    void initializeUs4oems(std::vector<IUs4OEMHandle> &ius4oems, int level) {
+        std::vector<std::future<void>> results;
+
+        for(IUs4OEMHandle &ius4oem : ius4oems) {
+            std::future<void> result = std::async(std::launch::async, [&ius4oem, level]() {ius4oem->Initialize(level);});
+            results.push_back(std::move(result));
+        }
+        for(auto &result: results) {
+            result.wait();
+            result.get(); // wait and throw exception if necessary.
         }
     }
 };
