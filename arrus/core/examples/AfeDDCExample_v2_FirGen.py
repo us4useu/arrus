b90d9a74a73ac02a576475740e891e64b82a4ac1
Piotr Jarosik <piotr.jarosik@us4us.eu>
2022-06-12 18:52:44 +0200

    Removed unnecessary semi-colons, minor refactoring.

--- arrus/core/examples/AfeDDCExample_v2_FirGen.py
+++ arrus/core/examples/AfeDDCExample_v2_FirGen.py
@@ -1,40 +1,36 @@
-import numpy as np;
-import scipy as sp;
+import numpy as np
+import scipy as sp
 import matplotlib.pyplot as plt
 import sys
 
-from scipy import signal;
+from scipy import signal
 
-if len(sys.argv)<3:
+if len(sys.argv) < 3:
     print('Usage: "DDCFirGen.py [decimation factor] [cutoff frequency]"')
-else: 
-    cutoff = float(sys.argv[2]);
-    M = float(sys.argv[1]);
-    
-    fs = 65;
+else:
+    cutoff = float(sys.argv[2])
+    M = float(sys.argv[1])
 
-    if M%1 == 0.0:
-        n = 16*M
-    elif M%1 == 0.5:
-        n = 32*M
-    elif M%1 == 0.25:
-        n = 64*M
-    elif M%1 == 0.75:
-        n = 64*M
-        
-    n = int(n);
-    
-    h = signal.firwin(n, cutoff/(fs/2));
-    Mh = max(h);
-    sf = 32624/Mh;
-    h2 = sf*h;
-    h3 = h2[int(n/2):];
-    
+    fs = 65
+
+    if M % 1 == 0.0:
+        n = 16 * M
+    elif M % 1 == 0.5:
+        n = 32 * M
+    elif M % 1 == 0.25:
+        n = 64 * M
+    elif M % 1 == 0.75:
+        n = 64 * M
+
+    n = int(n)
+
+    h = signal.firwin(n, cutoff / (fs / 2))
+    Mh = max(h)
+    sf = 32624 / Mh
+    h2 = sf * h
+    h3 = h2[n//2:]
+
     out = open("fir_generated.txt", "w")
-    for n in range (0, int(n/2)):
-        out.write(str(int(h3[n])));
-        out.write('\n');
- 
-
-
-
+    for i in range(0, n//2):
+        out.write(str(int(h3[i])))
+        out.write('\n')
