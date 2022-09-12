c6ccc7efe7efaa6d8d94fb69a2ea582a0be842f0
Piotr Jarosik <piotr.jarosik@us4us.eu>
2022-09-12 19:17:04 +0200

    Fixed unit tests.

--- arrus/core/devices/us4r/probeadapter/ProbeAdapterImplTest.cpp
+++ arrus/core/devices/us4r/probeadapter/ProbeAdapterImplTest.cpp
@@ -116,6 +116,7 @@ public:
     MOCK_METHOD(void, disableAfeDemod, (), (override));
     MOCK_METHOD(uint16_t, getAfe, (uint8_t address), (override));
     MOCK_METHOD(void, setAfe, (uint8_t address, uint16_t value), (override));
+    MOCK_METHOD(float, getUCDMeasuredVoltage, (uint8_t), (override));
 };
 
 class AbstractProbeAdapterImplTest : public ::testing::Test {
