b2d35d686e41200c0272cd410c001679c51ff28e
Piotr Jarosik <piotr.jarosik@us4us.eu>
2022-11-23 13:02:52 +0100

    Merge fix.

--- arrus/core/devices/us4r/Us4RImpl.h
+++ arrus/core/devices/us4r/Us4RImpl.h
@@ -149,8 +149,6 @@ private:
      */
     void applyForAllUs4OEMs(const std::function<void(Us4OEM* us4oem)>& func, const std::string &funcName);
 
-    void checkVoltage(Voltage voltage, float tolerance, float(Us4RImpl::func));
-
     void disableAfeDemod();
     void setAfeDemod(float demodulationFrequency, float decimationFactor, const float *firCoefficients,
                      size_t nCoefficients);
