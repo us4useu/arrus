a02b602364d4f60ef01c8b56456d54ce7eb932dc
Piotr Jarosik <piotr.jarosik@us4us.eu>
2023-02-20 09:02:41 +0100

    Updated custom tx/rx sequence example: using us4R linear TGC.

--- api/python/examples/custom_tx_rx_sequence.py
+++ api/python/examples/custom_tx_rx_sequence.py
@@ -11,6 +11,8 @@ import arrus.utils.imaging
 import arrus.utils.us4r
 import queue
 import numpy as np
+import arrus.ops.tgc
+import arrus.medium
 
 from arrus.ops.us4r import (
     Scheme,
@@ -37,9 +39,10 @@ arrus.add_log_file("test.log", arrus.logging.INFO)
 
 def main():
     # Here starts communication with the device.
-    with arrus.Session() as sess:
+    medium = arrus.medium.Medium(name="water", speed_of_sound=1490)
+    with arrus.Session("C:/Users/Public/us4r.prototxt", medium=medium) as sess:
         us4r = sess.get_device("/Us4R:0")
-        us4r.set_hv_voltage(20)
+        us4r.set_hv_voltage(5)
 
         n_elements = us4r.get_probe_model().n_elements
         # Full transmit aperture, full receive aperture.
@@ -69,7 +72,7 @@ def main():
                 ),
             ],
             # Turn off TGC.
-            tgc_curve=[14]*200,  # [dB]
+            tgc_curve=[],  # [dB]
             # Time between consecutive acquisitions, i.e. 1/frame rate.
             sri=50e-3
         )
@@ -91,6 +94,7 @@ def main():
         )
         # Upload the scheme on the us4r-lite device.
         buffer, metadata = sess.upload(scheme)
+        us4r.set_tgc(arrus.ops.tgc.LinearTgc(start=34, slope=2e2))
         # Created 2D image display.
         display = Display2D(metadata=metadata, value_range=(-1000, 1000))
         # Start the scheme.
