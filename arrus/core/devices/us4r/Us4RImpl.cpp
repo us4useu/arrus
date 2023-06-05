1ac10ce8d0baa8ead22dc7ddac5bc4ceae039212
jrozb91 <55283527+jrozb91@users.noreply.github.com>
2023-06-05 13:08:55 +0200

    Removed OEM+ HV voltages check (NYI)

--- arrus/core/devices/us4r/Us4RImpl.cpp
+++ arrus/core/devices/us4r/Us4RImpl.cpp
@@ -70,14 +70,15 @@ std::vector<std::pair <std::string,float>> Us4RImpl::logVoltages(bool isUS4PSC) 
     }
 
     //Verify measured voltages on OEMs
-    for (uint8_t i = 0; i < getNumberOfUs4OEMs(); i++) {
+    //HV measurements NYI in OEM+
+    /*for (uint8_t i = 0; i < getNumberOfUs4OEMs(); i++) {
         voltage = this->getUCDMeasuredHVPVoltage(i);
         temp = std::make_pair(std::string("HVP on OEM#" + std::to_string(i)), voltage);
         voltages.push_back(temp);
         voltage = this->getUCDMeasuredHVMVoltage(i);
         temp = std::make_pair(std::string("HVM on OEM#" + std::to_string(i)), voltage);
         voltages.push_back(temp);
-    }
+    }*/
 
     return voltages;
 }
