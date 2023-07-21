6382106475a7ff94b091d3a0360f9a2ee38f152c
zkLog <ziemowit.klimonda@us4us.eu>
2023-07-21 10:31:54 +0200

    Dictionary of known media introduced

--- api/python/arrus/medium.py
+++ api/python/arrus/medium.py
@@ -9,3 +9,16 @@ class Medium:
 class MediumDTO:
     name: str
     speed_of_sound: float
+
+media = dict(
+    (m.name, m) for m in [
+        Medium(name="water", speed_of_sound=1490),
+        Medium(name="steel", speed_of_sound=5960),
+        Medium(name="ats549", speed_of_sound=1450),
+    ]
+)
+
+
+def get_medium_by_name(name: str):
+    return media[name]
+
