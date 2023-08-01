84e11b79e546579cb4ed1dfa4f1ff011f77c6034
zkLog <ziemowit.klimonda@us4us.eu>
2023-08-01 19:55:49 +0200

    dB/MHz/cm -> dB/Hz/m

--- api/python/arrus/medium.py
+++ api/python/arrus/medium.py
@@ -7,13 +7,14 @@ class Medium:
     The attenuation parameters are associated with the assumption that
     atteuation coefficient alpha is given by following equation
 
-                    alpha = a*f^n
+                    alpha = a*(f)^n
 
     where a is attenuation coefficient at 1MHz, f is frequency (in MHz),
     and n is dimensionless exponent.
+    NOTE: In arrus SI units are used.
     :param name: medium unique name
     :param speed_of_sound: longitudinal wave propagation speed in [m/s]
-    :param attenuation_a: atteunation coefficient at 1MHz in [dB/MHz/cm]
+    :param attenuation_a: attenuation coefficient at 1MHz in [dB/Hz/m]
     :param atteunation_n: dimensionless exponent determining the attenuation frequency dependence
     """
     name: str
@@ -43,13 +44,13 @@ media = dict(
         Medium(
             name="ats549",
             speed_of_sound=1450,
-            attenuation_a=0.5,
+            attenuation_a=0.5e-4,
             attenuation_n=1,
         ),
         Medium(
             name="soft_tissue",
             speed_of_sound=1540,
-            attenuation_a=0.5,
+            attenuation_a=0.5e-4,
             attenuation_n=1,
         ),
     ]
