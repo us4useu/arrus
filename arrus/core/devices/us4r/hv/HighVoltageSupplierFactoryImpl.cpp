e7a903bc08bc0d261291dee8762eee305943be29
jrozb91 <55283527+jrozb91@users.noreply.github.com>
2023-08-28 10:02:35 +0200

    Added hvps support

--- arrus/core/devices/us4r/hv/HighVoltageSupplierFactoryImpl.cpp
+++ arrus/core/devices/us4r/hv/HighVoltageSupplierFactoryImpl.cpp
@@ -28,13 +28,7 @@ HighVoltageSupplierFactoryImpl::getHighVoltageSupplier(const HVSettings &setting
     DeviceId id(DeviceType::HV, 0);
 
     if(name == "hv256")  {
-        std::unique_ptr<IDBAR> dbar(GetDBARLite(dynamic_cast<II2CMaster *>(master)));
-        std::unique_ptr<IHV> hv(GetHV256(dbar->GetI2CHV(), std::move(logger)));
-        return std::make_unique<HighVoltageSupplier>(id, settings.getModelId(), std::move(dbar), std::move(hv));
-    }
-    else if(name == "hv256p")  {
-        // TODO how to detect if we have DBAR-Lite PCIE or the legacy DBAR-Lite?
-        std::unique_ptr<IDBAR> dbar(GetDBARLitePcie(dynamic_cast<II2CMaster *>(master)));
+        std::unique_ptr<IDBAR> dbar(GetDBARLite(dynamic_cast<II2CMaster *>(us4oems[0])));
         std::unique_ptr<IHV> hv(GetHV256(dbar->GetI2CHV(), std::move(logger)));
         return std::make_unique<HighVoltageSupplier>(id, settings.getModelId(), std::move(dbar), std::move(hv));
     }
@@ -47,12 +41,16 @@ HighVoltageSupplierFactoryImpl::getHighVoltageSupplier(const HVSettings &setting
     else if(name == "us4rpsc") {
         // TODO?
         throw std::runtime_error("unsupported: " + name + " please set hv: hv256-pcie in the prototxt file");
-//        std::unique_ptr<IDBAR> dbar(GetUs4RDBAR(dynamic_cast<II2CMaster *>(master)));
 //        std::unique_ptr<IHV> hv(GetUs4RPSC(dbar->GetI2CHV(), std::move(logger)));
 //        return std::make_unique<HighVoltageSupplier>(id, settings.getModelId(), std::move(dbar), std::move(hv));
     }
     else if(name == "us4oemhvps") {
-        //TO DO
+        std::vector<HighVoltageSupplier::Handle> hvs(us4oems.size(), nullptr);
+        std::transform(std::begin(us4oems), std::end(us4oems), std::begin(hvs), [](IUs4OEM* us4oem){
+            return std::make_unique<HighVoltageSupplier::Handle>(us4oem->getHVPS());
+        }); 
+        return hvs;
+
     }
     else {
         throw IllegalArgumentException(
