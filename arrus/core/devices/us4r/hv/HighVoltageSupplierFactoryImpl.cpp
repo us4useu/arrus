0d4ac8f7312bccf98b595a5ef4882d4eaf668931
jrozb91 <55283527+jrozb91@users.noreply.github.com>
2023-09-21 11:12:42 +0200

    Added DBARLite detection based on us4oem version

--- arrus/core/devices/us4r/hv/HighVoltageSupplierFactoryImpl.cpp
+++ arrus/core/devices/us4r/hv/HighVoltageSupplierFactoryImpl.cpp
@@ -27,26 +27,26 @@ HighVoltageSupplierFactoryImpl::getHighVoltageSupplier(const HVSettings &setting
     us4r::Logger::Handle logger = std::make_unique<Us4RLoggerWrapper>(arrusLogger);
     DeviceId id(DeviceType::HV, 0);
 
-    if(name == "hv256")  {
-        std::unique_ptr<IDBAR> dbar(GetDBARLite(dynamic_cast<II2CMaster *>(us4oems[0])));
-        std::unique_ptr<IHV> hv(GetHV256(dbar->GetI2CHV(), std::move(logger)));
-        
-        auto _hv = std::make_unique<HighVoltageSupplier>(id, settings.getModelId(), std::move(dbar), std::move(hv));
+    if(name == "hv256" || name == "hv256p")  {
 
         std::vector<HighVoltageSupplier::Handle> hvs;
-        hvs.push_back(std::move(_hv));
+        auto ver = us4oems[0]->GetOemVersion();
 
-        return hvs;
-    }
-    else if(name == "hv256p")  {
-        // TODO how to detect if we have DBAR-Lite PCIE or the legacy DBAR-Lite?
-        std::unique_ptr<IDBAR> dbar(GetDBARLitePcie(dynamic_cast<II2CMaster *>(us4oems[0])));
-        std::unique_ptr<IHV> hv(GetHV256(dbar->GetI2CHV(), std::move(logger)));
-        
-        auto _hv = std::make_unique<HighVoltageSupplier>(id, settings.getModelId(), std::move(dbar), std::move(hv));
-
-        std::vector<HighVoltageSupplier::Handle> hvs;
-        hvs.push_back(std::move(_hv));
+        if(ver == 1) {
+            std::unique_ptr<IDBAR> dbar(GetDBARLite(dynamic_cast<II2CMaster *>(us4oems[0])));
+            std::unique_ptr<IHV> hv(GetHV256(dbar->GetI2CHV(), std::move(logger)));
+            auto _hv = std::make_unique<HighVoltageSupplier>(id, settings.getModelId(), std::move(dbar), std::move(hv));
+            hvs.push_back(std::move(_hv));
+        }
+        else if(ver == 2) {
+            std::unique_ptr<IDBAR> dbar(GetDBARLitePcie(dynamic_cast<II2CMaster *>(us4oems[0])));
+            std::unique_ptr<IHV> hv(GetHV256(dbar->GetI2CHV(), std::move(logger)));
+            auto _hv = std::make_unique<HighVoltageSupplier>(id, settings.getModelId(), std::move(dbar), std::move(hv));
+            hvs.push_back(std::move(_hv));
+        }
+        else {
+            throw std::runtime_error("Invalid OEM version");
+        }
 
         return hvs;
     }
@@ -60,7 +60,6 @@ HighVoltageSupplierFactoryImpl::getHighVoltageSupplier(const HVSettings &setting
         std::vector<HighVoltageSupplier::Handle> hvs;
 
         for(auto us4oem: us4oems) {
-            std::unique_ptr<IDBAR> dbar;
             std::unique_ptr<IHV> hv(us4oem->getHVPS());
             hvs.push_back(std::move(std::make_unique<HighVoltageSupplier>(id, settings.getModelId(), std::nullopt, std::move(hv))));
         }
