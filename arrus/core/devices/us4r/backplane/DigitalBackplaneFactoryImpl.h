9214c62798513d9e79e97aac758f80a53d6b444d
Piotr Jarosik <piotr.jarosik@us4us.eu>
2023-11-22 14:40:30 +0100

    #US4R-352: Provided the possibility to specify what DBAR is used regardless of HV. (#316)

--- arrus/core/devices/us4r/backplane/DigitalBackplaneFactoryImpl.h
+++ arrus/core/devices/us4r/backplane/DigitalBackplaneFactoryImpl.h
@@ -13,35 +13,59 @@ namespace arrus::devices {
 
 class DigitalBackplaneFactoryImpl: public DigitalBackplaneFactory {
 public:
+
+    std::optional<DigitalBackplane::Handle> getDigitalBackplane(const DigitalBackplaneSettings &settings,
+                                                                const std::vector<IUs4OEM *> &us4oems) override {
+        const std::string &manufacturer = settings.getModelId().getManufacturer();
+        const std::string &name = settings.getModelId().getName();
+        if(manufacturer != "us4us") {
+            throw IllegalArgumentException("Only 'us4us' digital backplane is supported.");
+        }
+        if(name == "dbarlite")  {
+            return getDBARLite(us4oems);
+        }
+        else if(name == "us4rdbar") {
+            std::unique_ptr<IDBAR> dbar(GetUs4RDBAR(dynamic_cast<II2CMaster *>(us4oems[0])));
+            return std::make_unique<DigitalBackplane>(std::move(dbar));
+        }
+        else {
+            throw IllegalArgumentException(
+                ::arrus::format("Unrecognized DBAR: {}, {}", manufacturer, name));
+        }
+    }
+
     std::optional<DigitalBackplane::Handle> getDigitalBackplane(const HVSettings &settings,
                                                                 const std::vector<IUs4OEM *> &us4oems) override {
         const std::string &manufacturer = settings.getModelId().getManufacturer();
         const std::string &name = settings.getModelId().getName();
         if(name == "hv256")  {
-            auto ver = us4oems[0]->GetOemVersion();
-            if(ver == 1) {
-                std::unique_ptr<IDBAR> dbar(GetDBARLite(dynamic_cast<II2CMaster *>(us4oems[0])));
-                return std::make_unique<DigitalBackplane>(std::move(dbar));
-            }
-            else if(ver == 2) {
-                std::unique_ptr<IDBAR> dbar(GetDBARLitePcie(dynamic_cast<II2CMaster *>(us4oems[0])));
-                return std::make_unique<DigitalBackplane>(std::move(dbar));
-            }
-            else {
-                throw std::runtime_error("Invalid OEM version");
-            }
+            return getDBARLite(us4oems);
         }
         else if(name == "us4rpsc") {
             std::unique_ptr<IDBAR> dbar(GetUs4RDBAR(dynamic_cast<II2CMaster *>(us4oems[0])));
             return std::make_unique<DigitalBackplane>(std::move(dbar));
         }
         else if(name == "us4oemhvps") {
-            // TODO consider allowing DBAR even if us4oemhvps is used.
             return std::nullopt;
         }
         else {
             throw IllegalArgumentException(
                 ::arrus::format("Unrecognized high-voltage supplier: {}, {}", manufacturer, name));
+        }
+    }
+
+    std::optional<DigitalBackplane::Handle> getDBARLite(const std::vector<IUs4OEM *> &us4oems) const {
+        auto ver = us4oems[0]->GetOemVersion();
+        if(ver == 1) {
+            std::unique_ptr<IDBAR> dbar(GetDBARLite(dynamic_cast<II2CMaster *>(us4oems[0])));
+            return std::make_unique<DigitalBackplane>(std::move(dbar));
+        }
+        else if(ver == 2) {
+            std::unique_ptr<IDBAR> dbar(GetDBARLitePcie(dynamic_cast<II2CMaster *>(us4oems[0])));
+            return std::make_unique<DigitalBackplane>(std::move(dbar));
+        }
+        else {
+            throw std::runtime_error("Invalid OEM version");
         }
     }
 };
