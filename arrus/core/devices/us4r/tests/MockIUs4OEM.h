5cb641e2fe19d2400b239c48520b04ca7992c515
Piotr Jarosik <piotr.jarosik@us4us.eu>
2024-07-24 17:42:59 +0200

    Work in progress.

--- arrus/core/devices/us4r/tests/MockIUs4OEM.h
+++ arrus/core/devices/us4r/tests/MockIUs4OEM.h
@@ -237,6 +237,7 @@ public:
     MOCK_METHOD(void, SetTxVoltageLevel, (uint8_t level, uint16_t firing), (override));
     MOCK_METHOD(float, GetOCWSFrequency, (const float frequency), (override));
     MOCK_METHOD(void, LogPulsersInterruptRegister, (), (override));
+    MOCK_METHOD(void, BuildSequenceWaveforms, (bool verify), (override));
 };
 
 #define GET_MOCK_PTR(sptr) *(MockIUs4OEM *) (sptr.get())
