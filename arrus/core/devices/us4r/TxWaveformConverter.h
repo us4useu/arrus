5d231b1e31fa468d76fcf55f941025ddddd27518
Piotr Jarosik <jarosik.piotr@gmail.com>
2024-08-16 03:00:48 +0200

    Implemented TX pulse elongation.

--- arrus/core/devices/us4r/TxWaveformConverter.h
+++ arrus/core/devices/us4r/TxWaveformConverter.h
@@ -15,6 +15,16 @@ namespace arrus::devices {
 class TxWaveformConverter {
 public:
     constexpr static uint32_t END_STATE = 0b1110;
+    // Waveform sampling frequency
+    constexpr static float SAMPLING_FREQUENCY = 130e6f;
+    // Number of bits for the duration field
+    constexpr static uint32_t DURATION_BITS_SIZE = 7;
+    // MAXIMUM single duration; +1, because it's 127 + 2
+    constexpr static uint32_t MAX_N_CYCLES = (1 << (DURATION_BITS_SIZE)) + 1;
+    // MAX: 2^8-1+2
+    constexpr static uint32_t MAX_2_STATE_REPTITIONS = (1 << 8) + 1;
+    // MAX: 2^18-1+2, 18 = 3 + 5 + 5 + 5
+    constexpr static uint32_t MAX_4_STATE_REPTITIONS = (1 << 18) + 1;
 
     static void validateSegment(const ops::us4r::WaveformSegment &segment, const size_t nRepetitions) {
         auto segmentLength = segment.getState().size();
@@ -122,6 +132,141 @@ public:
                 }
             }
         }
+        else if (nRepeats == 1) {
+            std::vector<SegmentWithRepetitions> result;
+            // Provide the possibility to transmit with longer pulser (lower frequencies) by splitting and repeating
+            // it if it's necessary.
+            std::vector<float> currentDurations;
+            std::vector<int8> currentStates;
+            for(size_t i = 0; i < segment.getState().size(); ++i) {
+                auto d = segment.getDuration().at(i);
+                auto s = segment.getState().at(i);
+                auto durationCycles = toClk(d);
+                uint32_t reps = durationCycles / MAX_N_CYCLES;
+                uint32_t rem1 = 0;
+                uint32_t rem2 = durationCycles % MAX_N_CYCLES;
+                // Make sure rest >= 2
+                // NOTE: reps == 0 and rem2 == 1 is nto allowed for the pulser
+                if(reps > 0 && rem2 == 1) {
+                    // This in order to guarantee that rem2 is always >= 2
+                    rem2 = 2; // increase by 1
+                    rem1 = MAX_N_CYCLES-1; // Move one rep to rem1, and subtract 1 (from the rem2)
+                    reps -= 1; // one rep has been moved to the rem1
+                }
+
+                if(rem1 == 0 && reps == 0) {
+                    // duration short enough to be stored in a single waveform register
+                    // Just copy the current segment as it is.
+                    currentDurations.push_back(d);
+                    currentStates.push_back(s);
+                }
+                else if(reps <= 3) {
+                    for(uint32_t j = 0; j < reps; ++j) {
+                        currentDurations.push_back(toTime(MAX_N_CYCLES));
+                        currentStates.push_back(s);
+                    }
+                    if(rem1 > 0) {
+                        currentDurations.push_back(toTime(rem1));
+                        currentStates.push_back(s);
+                    }
+                    if(rem2 > 0) {
+                        currentDurations.push_back(toTime(rem2));
+                        currentStates.push_back(s);
+                    }
+                }
+                else {
+                    // reps >= 4 => try via 2-element segment repetitions (NOTE: the minimum number of 2-segment repetitions is 2!)
+                    if(!currentDurations.empty()) {
+                        // Close the currently visited elements as a single segment.
+                        result.emplace_back(
+                            ::arrus::ops::us4r::WaveformSegment(currentDurations, currentStates),
+                            size_t(1)
+                        );
+                        currentDurations.clear();
+                        currentStates.clear();
+                    }
+                    if(reps/2 <= MAX_2_STATE_REPTITIONS) {
+                        // 2-state repetition
+                        size_t segmentNReps = reps / 2;
+                        size_t repsRem = reps % 2;
+                        result.emplace_back(
+                            ::arrus::ops::us4r::WaveformSegment(
+                                {toTime(MAX_N_CYCLES), toTime(MAX_N_CYCLES)},
+                                {s, s}
+                            ),
+                            segmentNReps
+                        );
+                        if(repsRem > 0) {
+                            // repsRem == 1
+                            result.emplace_back(
+                                ::arrus::ops::us4r::WaveformSegment({toTime(MAX_N_CYCLES)}, {s}),
+                                1
+                            );
+                        }
+                        if (rem1 > 0) {
+                            result.emplace_back(
+                                ::arrus::ops::us4r::WaveformSegment({toTime(rem1)}, {s}),
+                                1
+                            );
+                        }
+                        if(rem2 > 0) {
+                            result.emplace_back(
+                                ::arrus::ops::us4r::WaveformSegment({toTime(rem2)}, {s}),
+                                1
+                            );
+                        }
+                    }
+                    else if (reps/4 <= MAX_4_STATE_REPTITIONS) {
+                        // 4-state repetition
+                        size_t segmentNReps = reps / 4;
+                        size_t repsRem = reps % 4;
+                        result.emplace_back(
+                            ::arrus::ops::us4r::WaveformSegment(
+                                {toTime(MAX_N_CYCLES), toTime(MAX_N_CYCLES), toTime(MAX_N_CYCLES), toTime(MAX_N_CYCLES)},
+                                {s, s, s, s}
+                                ),
+                            segmentNReps
+                        );
+                        if(repsRem > 0) {
+                            std::vector<float> ds(repsRem, toTime(MAX_N_CYCLES));
+                            std::vector<int8> ss(repsRem, s);
+                            result.emplace_back(
+                                ::arrus::ops::us4r::WaveformSegment(ds, ss),
+                                1
+                            );
+                        }
+                        if (rem1 > 0) {
+                            result.emplace_back(
+                                ::arrus::ops::us4r::WaveformSegment({toTime(rem1)}, {s}),
+                                1
+                            );
+                        }
+                        if(rem2 > 0) {
+                            result.emplace_back(
+                                ::arrus::ops::us4r::WaveformSegment({toTime(rem2)}, {s}),
+                                1
+                            );
+                        }
+                    }
+                    else {
+                        throw IllegalArgumentException(
+                            format("The TX waveform includes to long single state duration: {}", d)
+                       );
+                    }
+                }
+            }
+            // clean up anything was currently created.
+            if(!currentDurations.empty()) {
+                // Close the currently visited elements as a single segment.
+                result.emplace_back(
+                    ::arrus::ops::us4r::WaveformSegment(currentDurations, currentStates),
+                    1
+                );
+                currentDurations.clear();
+                currentStates.clear();
+            }
+            return result;
+        }
         else {
             // anything else: copy as it is
             // TODO: avoid copying?
@@ -189,7 +334,6 @@ public:
         return result;
     }
 private:
-    constexpr static float SAMPLING_FREQUENCY = 130e6f;
 
     static uint32_t toClk(float time) {
         return static_cast<uint32>(std::roundf(time*SAMPLING_FREQUENCY));
@@ -256,7 +400,7 @@ private:
     }
 
     static uint32_t setDuration(uint32_t input, uint32_t value) {
-        constexpr size_t size = 7;
+        constexpr size_t size = DURATION_BITS_SIZE;
         size_t maxValue = (1 << size);
         if(value >= maxValue) {
             throw IllegalArgumentException(format("Waveform number of cycles '{}' should be less than '{}'", value, maxValue));
