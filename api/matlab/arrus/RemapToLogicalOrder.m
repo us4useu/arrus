c69b7e7ebaa8b392d118e21c7c69406e0ad5a1a5
PiotrKarwat <55211251+PiotrKarwat@users.noreply.github.com>
2024-10-31 14:35:58 +0100

    #ARRUS-360: Fix MATLAB API and examples.  (#347)
    * Tgc bug fix
    
    tgcPoints must be in seconds for later use
    
    * Renaming of the example scripts
    
    * RemapToLogicalOrder: remove unnecessary properties
    
    * RemapToLogicalOrder: some code reorganization
    
    * Update CMakeLists.txt
    
    * BuildSequenceWaveform only for OEM+

--- api/matlab/arrus/RemapToLogicalOrder.m
+++ api/matlab/arrus/RemapToLogicalOrder.m
@@ -2,57 +2,52 @@ classdef RemapToLogicalOrder
     % Remaps from system physical order to the probe-like logical order.
 
     properties
-        nRep
+        nRx
         nTx
-        rxApSize
+        nRep
         hwDdcEnable
         reorgMap
-        framesOffset
-        framesNumber
-        oemId
-        frameId
-        channelId
-        rxTimeOffset
     end
 
     methods
         function obj = RemapToLogicalOrder(scheme, metadata)
+            
             addpath([fileparts(mfilename('fullpath')) '\mexcuda']);
-            obj.framesOffset = metadata{1};
-            obj.framesNumber = metadata{2};
-            obj.oemId = metadata{3};
-            obj.frameId = metadata{4};
-            obj.channelId = metadata{5};
-            obj.rxTimeOffset = metadata{6};
-
-            obj.nRep = scheme.txRxSequence.nRepeats;
-            obj.nTx = length(scheme.txRxSequence.ops);
-            obj.rxApSize = sum(scheme.txRxSequence.ops(1).rx.aperture);
-            obj.hwDdcEnable = ~isempty(scheme.digitalDownConversion);
-
-            nOem = numel(obj.framesNumber);
-            nChunk = sum(obj.framesNumber);
+            
+            framesOffset = metadata{1};
+            framesNumber = metadata{2};
+            oemId = metadata{3};
+            frameId = metadata{4};
+            channelId = metadata{5};
+            
+            nRx = sum(scheme.txRxSequence.ops(1).rx.aperture);
+            nTx = length(scheme.txRxSequence.ops);
+            nRep = scheme.txRxSequence.nRepeats;
+            nOem = numel(framesNumber);
+            nChunk = sum(framesNumber);
             nChan = 32;
-            nRep = obj.nRep;
-            nRx = obj.rxApSize;
-            nTx = obj.nTx;
+            
+            obj.nRx = uint32(nRx);
+            obj.nTx = uint32(nTx);
+            obj.nRep = uint32(nRep);
+            obj.hwDdcEnable = ~isempty(scheme.digitalDownConversion);
             
             obj.reorgMap = - ones(nChan, nChunk, 'int32');
             
             for iOem=1:nOem
-                nFrame = obj.framesNumber(iOem) / nRep;
+                nFrame = framesNumber(iOem) / nRep;
                 for iFrame=1:nFrame
-                    isSelect = obj.oemId == iOem-1 ...
-                             & obj.frameId == iFrame-1 ...
-                             & obj.channelId >= 0;
+                    isSelect = oemId == iOem-1 ...
+                             & frameId == iFrame-1 ...
+                             & channelId >= 0;
                     iTx = find(any(isSelect));
-                    iRx = find(isSelect(:,iTx) & obj.channelId(:,iTx) >= 0);
-                    iChan = obj.channelId(iRx,iTx) + 1;
+                    iRx = find(isSelect(:,iTx) & channelId(:,iTx) >= 0);
+                    iChan = channelId(iRx,iTx) + 1;
                     for iRep=1:nRep
-                        iChunk = obj.framesOffset(iOem) ...
-                               + obj.framesNumber(iOem) / nRep * (iRep-1) ...
+                        iChunk = framesOffset(iOem) ...
+                               + framesNumber(iOem) / nRep * (iRep-1) ...
                                + iFrame;
-
+                        
                         % 0-based indexing
                         obj.reorgMap(iChan,iChunk) = (iRep-1)*nTx*nRx + (iTx-1)*nRx + iRx-1; 
                     end
@@ -63,13 +58,12 @@ classdef RemapToLogicalOrder
         function dataOut = process(obj, dataIn)
             dataIn = gpuArray(dataIn);
 
-            dataOut = rawReorg( ...
-                dataIn, ...
-                obj.reorgMap, ...
-                uint32(obj.rxApSize), ...
-                uint32(obj.nTx), ...
-                uint32(obj.nRep), ...
-                obj.hwDdcEnable);
+            dataOut = rawReorg( dataIn, ...
+                                obj.reorgMap, ...
+                                obj.nRx, ...
+                                obj.nTx, ...
+                                obj.nRep, ...
+                                obj.hwDdcEnable);
         end
     end
 end
