f01d045f2902b58c5da2ce2e48626287bdccf92a
Piotr Jarosik <jarosik.piotr@gmail.com>
2024-12-20 10:58:57 +0100

    Version 0.10.5

--- devops.py
+++ devops.py
@@ -10,7 +10,7 @@ version = "0.2.0"
 # US4R_API_RELEASE_DIR, if the us4r_api_dir parameters is not provided
 # explicitly. Note: the below tag/branch should conform with the us4R required
 # version.
-us4r_api_default_branch_tag = "v0.11.4"
+us4r_api_default_branch_tag = "v0.11.5"
 
 
 def get_default_generator_for_current_os():
