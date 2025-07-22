a7e6615a99a23de7843730ca6eb38a546a415b54
Piotr Jarosik <piotr.jarosik@us4us.eu>
2025-07-22 12:39:18 +0200

    Fixed unit tests.

--- arrus/core/devices/us4r/tests/MockIUs4OEM.h
+++ arrus/core/devices/us4r/tests/MockIUs4OEM.h
@@ -235,6 +235,9 @@ public:
     MOCK_METHOD(void, EnableSystemInterrupts, (const IUs4OEM::CallbacksMap&), (override));
     MOCK_METHOD(void, ResetDMACallbacks, (), (override));
     MOCK_METHOD(void, SetPulserInterruptCallback, (const std::function<void()> &), (override));
+    MOCK_METHOD(void, EraseHVPSCalibration, (), (override));
+    MOCK_METHOD(void, CalibrateHVPS, (), (override));
+    MOCK_METHOD(bool, CheckHVPSCalibration, (), (override));
     MOCK_METHOD(void, BuildSequenceWaveform, (const unsigned short), (override));
 };
 
