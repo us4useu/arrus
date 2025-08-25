d8b1e26498678fd5f9744e1847c84e5b4e85bdac
mateusz-us4us <mateusz@us4us.eu>
2025-08-25 11:10:48 +0200

    Fixed build on Windows.

--- arrus/core/api/ops/us4r/Scheme.h
+++ arrus/core/api/ops/us4r/Scheme.h
@@ -156,8 +156,7 @@ public:
     SchemeBuilder& withWorkMode(Scheme::WorkMode mode);
     ARRUS_CPP_EXPORT
     SchemeBuilder& withDigitalDownConversion(DigitalDownConversion ddc);
-    ARRUS_CPP_EXPORT
-
+    
     ARRUS_CPP_EXPORT
     Scheme build();
 private:
