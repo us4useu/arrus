c307f50899b44af0d8d6a3c301eb0050f1cfec2d
PiotrKarwat <piotr.karwat@us4us.eu>
2025-11-17 14:27:23 +0100

    Adjust log-path to OS

--- api/matlab/arrus/Us4R.m
+++ api/matlab/arrus/Us4R.m
@@ -959,7 +959,7 @@ classdef Us4R < handle
             end
 
             % Initialization
-            arrus.initialize("clogLevel", "INFO", "logFilePath", "C:/Temp/arrus.log", "logFileLevel", "TRACE");
+            arrus.initialize("clogLevel", "INFO", "logFilePath", getLogPath(), "logFileLevel", "TRACE");
 
             obj.session = arrus.session.Session(configFile);
             obj.us4r = obj.session.getDevice("/Us4R:0");
@@ -1037,6 +1037,38 @@ classdef Us4R < handle
 
             obj.sys.isHardwareProgrammed = false;
 
+        end
+
+        function logPath = getLogPath()
+
+            if ispc
+                % Windows
+                baseDir = getenv('LOCALAPPDATA');
+                if ~isempty(baseDir)
+                    logDir = fullfile(baseDir, 'arrus', 'logs');
+                else
+                    logDir = fullfile(pwd, 'logs');
+                    warning(['Default log file location is not recognized, log file will be saved to ' logDir]);
+                end
+            elseif isunix && ~ismac
+                % Linux
+                baseDir = getenv('HOME');
+                if ~isempty(baseDir)
+                    logDir = fullfile(baseDir, '.arrus', 'logs');
+                else
+                    logDir = fullfile(pwd, 'logs');
+                    warning(['Default log file location is not recognized, log file will be saved to ' logDir]);
+                end
+            else
+                error('Unsupported OS');
+            end
+
+            if ~exist(logDir, 'dir')
+                mkdir(logDir);
+            end
+
+            logPath = fullfile(logDir, 'arrus.log');
+            
         end
 
         function seqOut = mergeSequences(obj,seqIn)
